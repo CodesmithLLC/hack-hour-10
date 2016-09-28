1a13afdf86776ecaf556a994b76277b0f0f445fc
Martin-Ting <martinhting@gmail.com>
2016-09-28 09:53:47 -0700

    finish fizzbuzz

---
+++ challenges/hackhour1test.js
@@ -0,0 +1,18 @@
+'use strict';
+let chai = require('chai');
+let expect = chai.expect;
+
+let fizzbuzz = require('./hackhour1.js');
+fizzbuzz = fizzbuzz.fizzbuzz;
+
+
+describe('fizzbuzzTesting', function(){
+	it('fizzbuzz(5) should return: 1 2 fizz 4 buzz', function(){
+		let fizzbuzz5 = fizzbuzz(5);
+		expect(fizzbuzz5).to.equal('1\n2\nfizz\n4\nbuzz');
+	});
+	it('fizzbuzz(16) should return: 1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16',function(){
+		let fizzbuzz16 = fizzbuzz(16);
+		expect(fizzbuzz16).to.equal('1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizzbuzz\n16');
+	});
+});
