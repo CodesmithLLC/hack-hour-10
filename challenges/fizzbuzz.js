d7f6af7f16c396c02b6c14e21498f7276c5b24d7
Ahmad K <lance@codemosey.net>
2016-09-28 09:54:30 -0700

    solution one

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,20 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {
-
+function fizzbuzz(endNumber) {
+  const result = [];
+  for (let currentNumber = 1; currentNumber <= endNumber; currentNumber++) {
+    if (currentNumber % 3 === 0 && currentNumber % 5 === 0) {
+      result.push('fizzbuzz');
+    } else if (currentNumber % 3 === 0) {
+      result.push('fizz');
+    } else if (currentNumber % 5 === 0) {
+      result.push('buzz');
+    } else result.push(currentNumber);
+  }
+    return result;
 }
+
+console.log(fizzbuzz(process.argv[2]));
 
 module.exports = fizzbuzz;
