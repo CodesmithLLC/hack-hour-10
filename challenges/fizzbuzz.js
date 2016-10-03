d47ae703890dd0e3ac41732cd20752f5d4c7839d
Ahmad K <lance@codemosey.net>
2016-10-03 12:19:56 -0700

    fixed typos

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-=======
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
-// fizzbuzz(16);  -> [ 1,
-//                     2,
-//                     'fizz',
-//                     4,
-//                     'buzz',
-//                     'fizz',
-//                     7,
-//                     8,
-//                     'fizz',
-//                     'buzz',
-//                     11,
-//                     'fizz',
-//                     13,
-//                     14,
-//                     'fizzbuzz',
-//                     16 ]
-
->>>>>>> d7f6af7f16c396c02b6c14e21498f7276c5b24d7
 function fizzbuzz(endNumber) {
   const result = [];
   for (let currentNumber = 1; currentNumber <= endNumber; currentNumber++) {
@@ -32,11 +11,4 @@ function fizzbuzz(endNumber) {
   }
     return result;
 }
-
-console.log(fizzbuzz(process.argv[2]));
-
-<<<<<<< HEAD
 module.exports = fizzbuzz;
-=======
-module.exports = fizzbuzz;
->>>>>>> d7f6af7f16c396c02b6c14e21498f7276c5b24d7
