1693962e97a2da14e85361c4d8c59b3edbb7382b
Martin-Ting <martinhting@gmail.com>
2016-10-04 09:34:52 -0700

    finish kthToLastNode HackHour

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,39 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  var kValue = 0;
+  var currNode = head;
+  var kthNode = head;
+  var listLength = 0;
+  // move currNode over k times
+  // A - B - C - D - E -
+  //     /\
+  while(kValue < k && currNode !== null){
+    listLength++;
+    //console.log("k is: " + k);
+    //console.log("currNode val is: " + currNode.value);
+    currNode = currNode.next;
+    kValue++;
+  }
 
+  //now iterate k until u is null to find kth from the end
+  // A - B - C - D - E -
+  //                 /\ <- k     |  last iteration before k is null
+  //             /\              |
+  while(currNode !== null){
+    kthNode = kthNode.next;
+    currNode = currNode.next;
+    listLength++;
+  }
+  //console.log("k: " + k);
+  //console.log("listlength: " + listLength);
+  if(k === 0 || k > listeLength){
+      return undefined;
+  }
+
+  return kthNode.value;
 }
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
