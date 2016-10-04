c2d38970f5713f4cbf441913a7a09c0893c33086
David Wilson <w.dave.w@gmail.com>
2016-10-04 10:05:57 -0700

    Add kth-to-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,32 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  const length = getLength(head);
+  if (k < 1 || k > length) return undefined;
+  for (let i = 0; i < length - k; i++) {
+    head = head.next;
+  }
+  return head.value;
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+function getLength(head) {
+  let length = 1;
+  while (head.next !== null) {
+    head = head.next;
+    length += 1;
+  }
+  return length;
+
+}
+  var a = new Node('A');
+  var b = new Node('B');
+  var c = new Node('C');
+  var d = new Node('D');
+  var e = new Node('E');
+ 
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  d.next = e;
+  
+//module.exports = {Node: Node, kthToLastNode: kthToLastNode};
