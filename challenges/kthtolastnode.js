b5b95016c73c6f091eb5cbc33a59c1567eccaba1
Ahmad K <lance@codemosey.net>
2016-10-04 09:38:21 -0700

    finished kthnodefromlast, added mode-mean solution to repo from partner's remote

---
+++ challenges/kthtolastnode.js
@@ -0,0 +1,55 @@
+/**
+ * Write a function that takes an integer and the head of a singly linked list,
+ * and returns the VALUE kth to last node in the list.
+ *
+ * var a = new Node('A');
+ * var b = new Node('B');
+ * var c = new Node('C');
+ * var d = new Node('D');
+ * var e = new Node('E');
+ *
+ * a.next = b;
+ * b.next = c;
+ * c.next = d;
+ * d.next = e;
+ *
+ * kthToLastNode(2,a); -> returns the node with the value 'D' (the second to last node)
+ */
+
+function Node(val) {
+  this.value = val;
+  this.next = null;
+}
+
+function kthToLastNode(k, head) {
+  let nodeStore = [];
+  let currentNode = head;
+  // When this loop exits, we should be holding the 'tail', and have a store of every object, indexed.
+  while (currentNode.next) {
+    nodeStore.push(currentNode);
+    currentNode = currentNode.next;
+  }
+  // Don't forget to push the tail!
+  nodeStore.push(currentNode);
+  console.log(nodeStore.length, k);
+  if (k >= nodeStore.length) return nodeStore[0];
+  else return nodeStore[nodeStore.length - k];
+}
+
+ var a = new Node('A');
+ var b = new Node('B');
+ var c = new Node('C');
+ var d = new Node('D');
+ var e = new Node('E');
+ 
+ a.next = b;
+ b.next = c;
+ c.next = d;
+ d.next = e;
+
+console.log(kthToLastNode(2, a));
+console.log(kthToLastNode(6, a));
+console.log(kthToLastNode(4, a));
+console.log(kthToLastNode(5, a));
+
+module.exports = {Node: Node, kthToLastNode: kthToLastNode};
