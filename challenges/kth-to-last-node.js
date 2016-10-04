b96486002629730eae5324226c714d6291975258
mlampedx <mlampesd@gmail.com>
2016-10-04 09:57:43 -0700

    kth-to-last-node complete

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -17,12 +17,40 @@
  */
 
 function Node(val) {
-  this.value = val;
-  this.next = null;
-}
+   this.value = val;
+   this.next = null;
+ }
 
-function kthToLastNode(k, head) {
+ function kthToLastNode(k, head) {
+   const len = findLength(head);
+   let counter = Number(len - k);
 
-}
+   if (k === 0 || k > len) return 'k must be a positive number that is less than the length';
+   if (head.constructor !== Node) return 'head must be a node';
+   if (len === 1) return head.value;
+
+ function find(node) {
+   if (counter === 0) return node.value;
+   if (node.next === null) return -1;
+
+   counter--;
+   return find (node.next);
+ }
+
+   return find (head);
+ }
+
+ function findLength(head) {
+   let length = 1;
+
+ function locateLen(node) {
+   if (node.next === null) return length;
+
+   length++;
+   return locateLen (node.next);
+   }
+
+   return locateLen(head);
+ }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
