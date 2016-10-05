45920c0f362400334a7d75e0245d3a13ae30b53a
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-05 09:32:02 -0700

    exponent hack hour

--- challenges/pow.js
+++ challenges/pow.js
@@ -1,9 +1,18 @@
 /* Write a function that calculates x^y, where x is given as the base and y is given as the power.
  * Use recursion!
+ *
+ * @param {number, number} First parameter is any non-0 non-infinity value. Second parameter
+ * 						   must be an integer.
+ * @return {number|undefined} Return number if given proper params. Otherwise return undefined.
  */
 
 function pow(base, power) {
-
+	
+	// Edge cases: base 0, user not including base or power, non-integer power values, no infinities
+	if (!base || !isFinite(base) || !Number.isInteger(power)) return undefined;
+	
+	if (power === 0) return 1;
+	return base * pow(base, power - 1);
 }
 
 module.exports = pow;
