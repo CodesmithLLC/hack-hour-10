9327505de9ad70369f37780d419289ecdd13c7f9
Dan <dan.zot@gmail.com>
2016-10-05 09:51:49 -0700

    complete challenge. TODO: test for boolean input

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,47 +11,76 @@
 
 
 function modemean(array) {
+  // helper func to calc mean
+  function mean(array) {
+    let sum = array.reduce((acc, cur) => acc += cur);
+    return Math.floor(sum/array.length);
+  }
+  // helper func to calc mode
+  function mode(array) {
+    const freq = {};
+    return array.reduce((mode, num) => {
+      // update freq table
+      !freq[num] ? freq[num] = 1 : freq[num]++;
+
+      // if first elem, set curr num to mode and set mode freq to 1
+      if (!mode) {
+        mode = num;
+        freq[mode] = 1;
+      }
+
+      // if freq of curr num is greater than the freq of the curr mode
+      // OR if freq of curr num is equal to the freq of the curr mode AND val of num > val of mode
+      // THEN set val of mode to the curr num
+      if (freq[num] > freq[mode] || freq[num] === freq[mode] && num > mode) return num;
+
+      // otherwise, continue passing along curr mode val
+      return mode;
+    }, undefined);
+  }
 
   if (array.length === 0) return;
-
-  function mode(array) {
-    const obj = {};
-    array.forEach(function(el) {
-      if (obj[el]) {
-        obj[el]++;
-      } else {
-        obj[el] = 1;
-      }
-    });
-    let max = 0;
-    let maxKey = 0;
-    for (let key in obj) {
-      if (obj[key] > max) {
-        max = obj[key];
-        maxKey = key;
-      }
-      if (obj[key] === max) {
-        if (key > maxKey) {
-          max = obj[key];
-          maxKey = key;
-        }
-      }
-    }
-    return maxKey;
-  }
-
-  function mean(array) {
-    let sum = array.reduce(function(acc, cur) {
-     return acc += cur;
-    });
-    return Math.floor(sum/(array.length));
-  }
-
-  let modeRes = mode(array);
-  let meanRes = mean(array);
-
-  return (modeRes == meanRes) ;
-
+  return mode(array) === mean(array);
 }
+
+
+
+// two iterations version
+// function mode(array) {
+//   const freq = {};
+//   array.forEach(num => freq[num] ? freq[num]++ : freq[num] = 1);
+//   let max = 0, maxNum;
+//   for (let num in freq) {
+//     if (freq[num] > max || freq[num] === max && num > maxNum) {
+//       max = freq[num];
+//       maxNum = num;
+//     }
+//   }
+//   return +maxNum;
+// }
+
+// normal case
+let test = [5, 4, 4, 3];
+console.assert(mode(test) === 4)
+console.assert(mean(test) === 4)
+
+// higher val num with equal freq
+test.push(5);
+console.assert(mode(test) === 5)
+
+// lower val num with greater freq
+test.push(4)
+console.assert(mode(test) === 4)
+
+// negative values
+test = [-1, -1, 0];
+console.assert(mode(test) === -1)
+
+// empty array
+console.assert(!mode([]))
+
+// zero as mode
+test.push(0);
+console.assert(mode(test) === 0)
 
 module.exports = modemean;
--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,59 @@
  */
 
 function pow(base, power) {
+  // check for int input
+  // console.log(base, isNaN(base), power, isNaN(power));
+  if (isNaN(base) || isNaN(power)) return;
 
+  // check for positive power
+  if (power < 0) return;
+
+  // establish base cases
+  if (power === 0) return 1;
+  if (power === 1) return base;
+  if (base === 0) return 0; // optimization
+
+  // recursive case
+  return base * pow(base, power - 1);
 }
 
 module.exports = pow;
+
+// testing
+
+// normal cases
+let p = 1;
+console.assert(pow(2, p) === Math.pow(2, p));
+p++;
+console.assert(pow(2, p) === Math.pow(2, p));
+p++;
+console.assert(pow(2, p) === Math.pow(2, p));
+
+// edge cases
+console.assert(pow(2, 0) === Math.pow(2, 0));
+console.assert(pow(2, 0) === Math.pow(2, 0));
+console.assert(pow(0, 10000) === Math.pow(0, 10000));
+
+// undefined behavior
+// negative powers
+console.assert(pow(2, -1) === undefined);
+console.assert(pow(2, -1) === undefined);
+// string input
+console.assert(pow('hello', 4) === undefined);
+console.assert(pow(4, 'hi') === undefined);
+console.assert(pow('hey', 'hi') === undefined);
+// array input
+console.assert(pow([1, 2], 4) === undefined);
+console.assert(pow(4, [1, 2]) === undefined);
+console.assert(pow({A: 'B'}, [1, 2]) === undefined);
+// object input
+console.assert(pow({1: 2}, 4) === undefined);
+console.assert(pow(4, {1: 2}) === undefined);
+console.assert(pow({A: 'B'}, {1: 2}) === undefined);
+// boolean input
+// console.log(pow(true, 4) === undefined);
+// console.log(pow(2, false) === undefined);
+// console.log(pow(true, false) === undefined);
+
+// zero to the zero
+console.assert(pow(0, 0) === Math.pow(0, 0));
