d9fdda93defeae7ae9ba217b818174d76a9d98e4
ryanbas21 <ryanbas21@gmail.com>
2016-10-05 09:29:51 -0700

    submitting wed hack hours

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,20 @@
  */
 
 function pow(base, power) {
-
+    let exp = 1;
+    for (let i = 0; i < power; i++){
+        exp *= base;
+    }
+    if (power === 0 || base === 1) return 1;
+    if (base > 0 && power > 0) return exp
+    if (power < 0){
+        power *= -1;
+          for (let i = 0 ; i < power; i++ ){
+              exp *= base;
+          }
+        return 1 / exp;
+  }
+  if (base < 0) return exp;
 }
 
 module.exports = pow;
