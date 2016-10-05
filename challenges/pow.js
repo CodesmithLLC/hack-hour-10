cb434f4e88ce97422e997aaba273e64314a96f9b
Martin-Ting <martinhting@gmail.com>
2016-10-05 09:22:51 -0700

    edited for cleanliness

--- challenges/pow.js
+++ challenges/pow.js
@@ -1,10 +1,8 @@
 /* Write a function that calculates x^y, where x is given as the base and y is given as the power.
  * Use recursion!
  */
-
 function pow(base,power){
     var result;
-    
     if(power < 0) result = 1/pow(base,Math.abs(power));
     else result = recpow(base, power);
 
