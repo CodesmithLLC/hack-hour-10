19e43305a178f0b20c8702644c782525dd864ee5
Cheng Ly <chengsieuly@gmail.com>
2016-10-06 10:02:30 -0700

    feat: Finish HR W2D4 reverse-in-place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -2,9 +2,9 @@
  * Write a function to reverse an array in place
  *
  * "In place" means "without creating a new object in memory"
- * 
- * In some languages, strings are mutable (like in Ruby). In other languages, 
- * such as Python and Javascript, strings are immutable, meaning they cannot 
+ *
+ * In some languages, strings are mutable (like in Ruby). In other languages,
+ * such as Python and Javascript, strings are immutable, meaning they cannot
  * be changed after they're created.
  *
  * Since strings are immutable in javascript, we will be reversing an array of characters instead.
@@ -12,7 +12,18 @@
  */
 
 function reverseInPlace(array) {
+  if (!Array.isArray(array)) return new Error('expected input to be an array');
 
+  // Insight is that we can just swap beginning and ending
+  for (let i = 0, len = array.length; i < Math.floor(len/2); i++) {
+    const first = array[i];
+    const last = array[len - i - 1];
+
+  	array[len - i - 1] = first;
+  	array[i] = last;
+  }
+
+  return array;
 }
 
 module.exports = reverseInPlace;
