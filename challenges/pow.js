57798d71be48c9812911e19905cd1bc2a55378c6
srei <steferu@gmail.com>
2016-10-06 09:10:11 -0700

    changes

--- challenges/pow.js
+++ challenges/pow.js
@@ -4,8 +4,8 @@
 
 
 function pow(base, power) {
-	if(power <= 0) return 1;
-	return base * pow(base, power-1);
+	if (power <= 0) return 1;
+	return base * pow (base, power-1);
 }
 
 module.exports = pow;
