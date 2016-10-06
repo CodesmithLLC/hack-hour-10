cb23dace314645d96b563a025728911cd450859b
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-06 09:17:32 -0700

    reverses array in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -12,7 +12,15 @@
  */
 
 function reverseInPlace(array) {
+  if (array.length) return undefined;
 
+  for (let i = 0; i < array.length / 2; i++) {
+	let temp = array[i];
+	array[i] = array[array.length - i - 1];
+	array[array.length - i - 1] = temp;
+  }
+  
+  return array;
 }
 
 module.exports = reverseInPlace;
