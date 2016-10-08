b5321389e48d4b50d30f34350445f7663fe4f581
Dan <dan.zot@gmail.com>
2016-10-08 09:34:39 -0700

    complete reverse-in-place hack hour

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -12,7 +12,47 @@
  */
 
 function reverseInPlace(array) {
-
+  if (!canReverseInPlace(array)) return; 
+  let swapMem;
+  let lastIndex = array.length - 1;
+  for (let i = 0; i < Math.floor(lastIndex / 2); i++) {
+    swapMem = array[i];
+    array[i] = array[lastIndex - i];
+    array[lastIndex - i] = swapMem;
+  }
 }
+
+// returns true if the input is an array of chars (strings of length 1)
+function canReverseInPlace(array) {
+  if (!Array.isArray(array)) return;
+  return array.every(elem => typeof elem === 'string' && elem.length === 1);
+}
+
+// testing
+
+// odd length array
+let hello = ['a', 'b', 'c', 'd', 'e'];
+console.log(hello);
+reverseInPlace(hello);
+console.log(hello);
+
+// even length array
+hello = ['1', '2', '3', '4', '5'];
+hello.push('?');
+console.log(hello);
+reverseInPlace(hello);
+console.log(hello);
+
+// incorrect input
+hello = {yah: 'nah'}; // object
+console.assert(reverseInPlace(hello) === undefined);
+hello = 'hello'; // string
+console.assert(reverseInPlace(hello) === undefined);
+hello = 123; // number
+console.assert(reverseInPlace(hello) === undefined);
+hello = ['w', 0, 'w']; // mixed array (number)
+console.assert(reverseInPlace(hello) === undefined);
+hello = ['w', ['o'], 'w']; // mixed array (array)
+console.assert(reverseInPlace(hello) === undefined);
 
 module.exports = reverseInPlace;
