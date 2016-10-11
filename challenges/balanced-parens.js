48c19a99304097795cc0dbcf08251dead8130ded
David Wilson <w.dave.w@gmail.com>
2016-10-11 09:54:53 -0700

    Slightly cleaner balanced paren solution

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,52 +25,33 @@
  */
 
 function balancedParens(input) {
-  let unbalancedParens = 0, unbalancedBrackets = 0, unbalancedBraces = 0;
   let stack = []
-  // console.log(input);
   for (let i = 0; i < input.length; i++) {
-    // console.log(stack);
-    // console.log('input:', input[i]);
     switch (input[i]) {
       case '(':
-        unbalancedParens += 1;
-        stack.push(')');
-        break;
+        stack.push(')'); break;
       case ')':
-        unbalancedParens -= 1;
         let x = stack.pop();
-        if (x !== ')') return false;
+        if (x !== ')' || x === undefined) return false;
         break;
       case '[':
-        unbalancedBrackets += 1;
         stack.push(']');
         break;
       case ']':
-        unbalancedBrackets -= 1;
         let y = stack.pop();
-        if (y !== ']') return false;
+        if (y !== ']' || y === undefined) return false;
         break;
       case '{':
-        unbalancedBraces += 1;
-        stack.push('}')
-        break;
+        stack.push('}'); break;
       case '}':
-        unbalancedBraces -= 1;
         let z = stack.pop();
-        if (z !== '}') return false;
+        if (z !== '}' || z === undefined) return false;
         break;
     }
   }
-  if (unbalancedBrackets < 0 || unbalancedParens < 0 || unbalancedBraces < 0) return false;
+  return (stack.length === 0);
 
-  return !(unbalancedParens || unbalancedBrackets || unbalancedBraces);
 }
 
-console.log(balancedParens('(')) // false
-console.log(balancedParens('()')) // true
-console.log(balancedParens(')()')) // false
-console.log(balancedParens('(())')) // true
-console.log(balancedParens('[](){}')) // true
-console.log(balancedParens('[({})]')) // true
-console.log(balancedParens('[(]{)}')) // false
+
 module.exports = balancedParens;
