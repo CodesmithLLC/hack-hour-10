880221edd233c085f5a3a4a963b556c9c3e08d23
Dan <dan.zot@gmail.com>
2016-10-11 10:00:54 -0700

    complete balanced-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,62 @@
  */
 
 function balancedParens(input){
+  // check for invalid input
+  if (typeof input !== 'string') return undefined;
 
+  // push on all parens/brackets
+  const stack = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] === '[' || input[i] === ']' ||
+        input[i] === '(' || input[i] === ')' ||
+        input[i] === '{' || input[i] === '}')
+      stack.push(input[i]);
+  }
+
+  // if odd number, parens are unbalanced
+  if (stack.length % 2 === 1) return false;
+
+  // use another stack to match parens/brackets
+  const pending = [];
+  while (stack.length > 0) {
+    const topParen = stack.pop()
+    switch (topParen) {
+      // for each open paren, if there isn't an unmatched paren in pending, return false
+      case '(': if (pending.length === 0) return false;
+                else if (pending[pending.length - 1] === ')') pending.pop();
+                break;
+      // for each closing paren, if the next paren on stack is the matching paren, discard both
+      // otherwise push onto pending stack
+      case ')': if (stack[stack.length - 1] === '(') stack.pop();
+                else pending.push(topParen); break;
+      case '[': if (pending.length === 0) return false;
+                else if (pending[pending.length - 1] === ']') pending.pop();
+                break;
+      case ']': if (stack[stack.length - 1] === '[') stack.pop();
+                else pending.push(topParen); break;
+      case '{': if (pending.length === 0) return false;
+                else if (pending[pending.length - 1] === '}') pending.pop();
+                break;
+      case '}': if (stack[stack.length - 1] === '{') stack.pop();
+                else pending.push(topParen); break;
+    }
+  }
+
+  // if there are unmatched parens/brackets, return false
+  return stack.length === 0 && pending.length === 0;
 }
+
+console.assert(balancedParens('(') === false);  // false
+console.assert(balancedParens('()') === true); // true
+console.assert(balancedParens(')(') === false);  // false
+console.assert(balancedParens('(())') === true);  // true
+
+console.assert(balancedParens('[](){}') === true); // true
+console.assert(balancedParens('[({})]') === true);   // true
+console.assert(balancedParens('[(]{)}') === false); // false
+
+console.assert(balancedParens(' var wow  = { yo: thisIsAwesome() }') === true); // true
+console.assert(balancedParens(' var hubble = function() { telescopes.awesome();') === false); // false
+
 
 module.exports = balancedParens;
