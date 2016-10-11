dc85536eaab1500134e5ce35d21d756fee570887
pddivine <pddivine@gmail.com>
2016-10-11 09:52:03 -0700

    Challenge complete

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -1,3 +1,4 @@
+"use strict"
 /*
  * write a function that takes a string of text and returns true if
  * the parentheses are balanced and false otherwise.
@@ -25,7 +26,75 @@
  */
 
 function balancedParens(input){
+  if (typeof input !== 'string') { return undefined; }
+  const arr = input.split('');
+
+  const balance = {
+    paren     : 0,
+    bracket   : 0,
+    curly     : 0,
+  }
+  let mode = [];
+  for (let i = 0; i < arr.length; i++) {
+    const val = arr[i];
+
+    // Handle balance and mode violation    
+    if (val === ')') {
+      balance.paren -= 1;
+      if (mode[0] !== '(') { 
+        return false 
+      };
+      mode.shift();
+    }
+    
+    if (val === ']') {
+      balance.bracket -= 1;
+      if (mode[0] !== '[') { 
+        return false 
+      };
+      mode.shift();
+    }
+    
+    if (val === '}') {
+      balance.curly -= 1;
+      if (mode[0] !== '{') { 
+        return false 
+      };
+      mode.shift();
+    }
+
+    if (val === '(') {
+      balance.paren += 1;
+      mode.unshift('(');
+    }
+    if (val === '[') {
+      balance.bracket += 1;
+      mode.unshift('[');
+    }
+    if (val === '{') {
+      balance.curly += 1;
+      mode.unshift('{');
+    }
+  }
+  
+
+  return balance.paren + balance.bracket + balance.curly === 0;
 
 }
 
 module.exports = balancedParens;
+
+// console.log('---');
+// console.log(balancedParens('('));  // false
+// console.log(balancedParens('()')); // true
+// console.log(balancedParens(')('));  // false
+// console.log(balancedParens('(())'));  // true
+// console.log('---');
+// console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]'));   // true
+// console.log(balancedParens('[(]{)}')); // false
+// console.log('---');
+// console.log(balancedParens(7)); // undefined
+// console.log('---');
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
