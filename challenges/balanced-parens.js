6894cbee1ea01ad65d04164ab6319135825bd786
Cheng Ly <chengsieuly@gmail.com>
2016-10-11 09:34:44 -0700

    feat: HH W3D2 balance-parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -24,8 +24,36 @@
  *
  */
 
-function balancedParens(input){
+function balancedParens(input=''){
+  // We can use a stack to implement this algorithm
+  // The idea is to push opening characters to a stack and
+  // pop out of it when we see closing brackets. Popped brackets
+  // must match the closing brackets
+  // The algorithm will need to run at O(n) 
 
+  // Loop through the string and look for the three types of opening brackets,
+  // '[', '{', '('
+
+  let i = 0;
+  const len = input.length;
+
+  const brackets = [];
+  while (i < len) {
+    if (input[i] === '(') brackets.push('(');
+    if (input[i] === ')' && brackets.pop() !== '(') return false;
+    if (input[i] === '{') brackets.push('{');
+    if (input[i] === '}' && brackets.pop() !== '{') return false;
+    if (input[i] === '[') brackets.push('[');
+    if (input[i] === ']' && brackets.pop() !== '[') return false;
+    i++;
+  }
+
+  // Final check to see if there are any leftovers opening brackets in our stack
+  // If there are still left, this means that there are not enough closing brackets
+  if (brackets.length > 0) return false;
+
+  // Every is good!
+  return true;
 }
 
 module.exports = balancedParens;
