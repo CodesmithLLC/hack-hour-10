0e88a79fdaad559a870b4989c0879319df00e975
Martin-Ting <martinhting@gmail.com>
2016-10-14 10:01:00 -0700

    finish hack hour subset-sum oct 14

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -7,9 +7,43 @@
  * subsetSum([8, 2, 4, 12], 13) -> false
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
+"use strict";
 
 function subsetSum(array, target) {
-
+  if(array.length < 0){
+    return false;
+  }else if(array.length === 1){
+    return array[0] === target;
+  }
+  //let stack = [array[0]];
+  let ret = subsetRec(array, target, 1, array[0]);
+  function subsetRec(array, target, index, total, stack){
+    if(total === array[index]){
+      return true;
+    }
+    //first, recursively call subsetRec with all items from index+1 to end 
+    //       of array with the new sum total + array[i]
+    for(let i = index+1; i < array.length; ++i){
+      if(subsetRec(array, target, i, total + array[i])){
+        return true;
+      }
+    }
+    //base cases
+    // [1]: total === target so return true so inner if in for loop returns true
+    if(total === target){
+       //console.log(stack);
+       return true;
+    }
+    // [2]: total exceeds target so return false
+    if(total > target){
+      return false;
+    }
+    // [3]: return false for all other cases 
+    //      (typically total < target and index < length)
+    return false;
+  }
+  return ret;
 }
+
 
 module.exports = subsetSum;
