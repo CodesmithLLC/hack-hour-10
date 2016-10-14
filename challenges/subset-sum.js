665490301794d232bb9e83c2c90f0f7fcdbd0334
Dean Edwards <dmedwards4@gmail.com>
2016-10-14 10:01:04 -0700

    subset challenge

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,30 @@
  */
 
 function subsetSum(array, target) {
+  let sums = [];
+  let found = false;
+  let test = 0;
 
+  for (let i = 0; i < array.length; i++) {
+    sums.push(array[i]);
+    for (let j = 0; j < array.length; j++) {
+      if (array[i] === array[j]) continue;
+      if (test + array[j] <= target) {
+        test += array[j];
+        sums.push(test);
+      }
+    }
+    test = 0;
+  }
+
+  // console.log(sums);
+  sums.forEach(num => {
+    if (num === target) found = true;
+  });
+  return found;
 }
-
+console.log(subsetSum([3, 7, 4, 2], 5));
+ console.log(subsetSum([3, 34, 4, 12, 5, 12], 32));
+console.log(subsetSum([8, 2, 4, 12], 13));
+// console.log(subsetSum([8, -2, 1, -3], 6));
 module.exports = subsetSum;
