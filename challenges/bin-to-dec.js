8c2c38549cbf76b8768b287035beb9b871c3eef1
Cheng Ly <chengsieuly@gmail.com>
2016-10-15 10:08:54 -0700

    Finish HH W3D6 bin-to-dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -1,3 +1,4 @@
+'use strict';
 /* You are given a string that represents a binary number
  *
  * Write a function that converts the binary string to a decimal number
@@ -14,7 +15,18 @@
  */
 
 function binToDec(binary) {
+  var total = 0;
+  for (let i = binary.length - 1, counter = 0; i >= 0; --i, ++counter) {
+    if (binary[i] === '1') {
+      total += Math.pow(2, counter);
+    }
+  }
+  return total;
+}
 
+function decToBin(decimal) {
+  if (decimal === 1) return '1';
+  return (decimal % 2) + decToBin(Math.floor(decimal / 2));
 }
 
 module.exports = binToDec;
