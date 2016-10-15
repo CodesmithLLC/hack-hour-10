d55c4df4ac238771169b62030e7ef8e180fe0fb9
Martin-Ting <martinhting@gmail.com>
2016-10-15 09:47:12 -0700

    implemented a hex version for fun

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -25,4 +25,34 @@ function binToDec(binary) {
   return total;
 }
 
+let hexDict = {
+           0 : 0,
+           1 : 1,
+           2 : 2, 
+           3 : 3,
+           4 : 4,
+           5 : 5,
+           6 : 6,
+           7 : 7,
+           8 : 8,
+           9 : 9,
+           A : 10,
+           B : 11,
+           C : 12,
+           D : 13,
+           E : 14,
+           F : 15};
+function hexToDec(hex){
+  if(!hex) return 0;
+  //console.log(`hex: ${hex}`);
+  let total = 0;
+  for( let i = hex.length-1, power = 0; i >= 0; --i, ++power ){
+    //console.log(`i ${i} power ${power} on ${hex[i]}`);
+    //console.log(`I want to add ${hexDict[hex[i]] * Math.pow(16,power)}`);
+    total += hexDict[hex[i]] * Math.pow(16,power); 
+  }
+  //console.log(total);
+  return total;
+}
+
 module.exports = binToDec;
