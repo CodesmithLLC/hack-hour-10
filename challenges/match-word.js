044fbdcec84f82ed8692954de7f70969065c97c0
Eviscerare <edward.h.guo@gmail.com>
2016-10-18 14:32:57 -0700

    finished match word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,42 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  let strings = str.split(/[^a-zA-Z]/);
+  strings = strings.filter(x => { if (x) return x });
 
+  if (strings.length % 2) return false;
+  if (!strings.length) return true;
+
+  let stack = [];
+  let latest;
+  for (let i = 0; i < strings.length; i += 1) {
+    let string = strings[i].toLowerCase();
+    let reversed = string.split('').reverse().join('');
+    
+    if (stack.length === 0) {
+      latest = string;
+      stack.push(string);
+    }
+    else {
+      if (reversed === latest) {
+        stack.pop();
+        latest = stack[stack.length - 1];
+      } else {
+        stack.push(string);
+        latest = string;
+      }
+    }
+  }
+  return !(stack.length > 0);
 }
 
 module.exports = matchWord;
+
+console.log('expected: true, false, true, false, true, true\n', 'actual:',
+  matchWord('__END_DNE-----'),
+  matchWord('__ENDDNE__'),
+  matchWord('IF()()fi[]'),
+  matchWord('for__if__rof__fi'),
+  matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'),
+  matchWord('')
+);
