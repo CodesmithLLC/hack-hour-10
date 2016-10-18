ea76c147de7e15341b22a779adc1ce5fe1c2f7d0
travishuff <huff.travis@gmail.com>
2016-10-18 10:00:16 -0700

    match-word.js

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,33 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  if (str === '') return true;
+  strArr = str.toLowerCase().replace(/[^a-z+]+/gi, ' ').split(' ');
+  // console.log(strArr);
+  bracketArr = [];
+  for (let i = 0; i < strArr.length; i++) {
+    let itemReverse = strArr[i].split('').reverse().join('');
+    if (!bracketArr.includes(itemReverse) && strArr[i] !== '') {
+      bracketArr.push(strArr[i]);
+      // console.log(bracketArr);
+    } else if (bracketArr[bracketArr.length - 1] === itemReverse && strArr[i] !== '') {
+      // console.log(strArr[i]);
+      bracketArr.pop();
+      // console.log(bracketArr);
+      return true;
+    } else if (bracketArr.includes(itemReverse)) {
+      return false;
+    }
+  }
+  return false;
 
 }
+
+// console.log(matchWord('__END_DNE-----'));//  -> true
+// console.log(matchWord('__ENDDNE__'));//  -> false       (not separated by a space)
+// console.log(matchWord('IF()()fi[]'));//  -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi'));//  -> false     not properly closed. like ( [) ] 
+// console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));//  -> true
+// console.log(matchWord(''));//  -> true
 
 module.exports = matchWord;
