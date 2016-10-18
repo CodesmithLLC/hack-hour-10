a6baa93c0bc87a02afbe1dc0370a936b70cbe35c
Will Galebach <willbach@gmail.com>
2016-10-18 09:56:42 -0700

    finished matchword challenge using stack and regex

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -1,4 +1,8 @@
-// Some languages have "if" statements that are closed by "fi" instead of curly brackets. Or they close a "case" with "esac", i.e. the same keyword backwards. for this problem we'll check that all words in a string are "closed". Write a function that takes a string and returns true if every word is closed by its backwards counterpart. Words must be separated by space or punctuation.
+// Some languages have "if" statements that are closed by "fi" instead of curly brackets. 
+// Or they close a "case" with "esac", i.e. the same keyword backwards. 
+// for this problem we'll check that all words in a string are "closed". 
+// Write a function that takes a string and returns true if every word is closed by 
+// its backwards counterpart. Words must be separated by space or punctuation.
 
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
@@ -8,7 +12,23 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  function isBackward(string1, string2) {
+    return string1.toLowerCase() === string2.split('').reverse().join('').toLowerCase();
+  }
+  let tags = str.match(/([a-zA-Z]+)/g);
+  let closed = [];
+  closed.push(tags[0]);
 
+  for(let i = 1; i < tags.length; i++) {
+    if( isBackward(tags[i], closed[closed.length - 1]) ) {
+      closed.pop();
+    }
+    else {
+      closed.push(tags[i]);
+    }
+  }
+  return closed[0] === undefined;
 }
+
 
 module.exports = matchWord;
