ead2cb5f3895623900f6c8e2c5b2853719d16b98
David Wilson <w.dave.w@gmail.com>
2016-10-18 09:53:26 -0700

    match-word solution

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,7 +8,32 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  const wordsRegex = /[^a-z]*([a-z]+)[^a-z]*/gi
 
+  // extract all of the words into an array
+  const words = [];
+  for (let word = wordsRegex.exec(str); word !== null; word = wordsRegex.exec(str)) {
+    words.push(word[1]);
+  }
+
+  // check if words are balanced
+  let changed = true;
+  while (changed) {
+    let i = -1;
+    changed = false;
+
+    // check each pair of consecutive words and see if they match. if they do, then remove them and set changed = true
+    // this will signal to repeat the process again after reaching the end of the array
+    while (words[++i + 1] !== undefined) {
+      if (words[i].toLowerCase() === words[i + 1].split('').reverse().join('').toLowerCase()) {
+        words.splice(i, 2);
+        changed = true;
+      }
+    }
+  }
+  // after exiting the outer while loop, there are no more matches. check if everything matched.
+  return (words.length === 0);
 }
 
+// console.log(matchWord('while elihw try yrt if cat dog god try tac yrt fi'));
 module.exports = matchWord;
