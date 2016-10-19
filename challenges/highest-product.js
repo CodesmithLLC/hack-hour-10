3cdd48e47b2e2a09fcaa42c125fc67a07e923f96
Cheng Ly <chengsieuly@gmail.com>
2016-10-19 10:05:05 -0700

    Finish HH W3D3 highest-product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,34 @@
  */
 
 function highestProduct(array) {
+  // What if it is an empty array
+  if (array.length < 1) return;
 
+  // Basically grab the three highest numbers from the array and multiply them together
+  // We need a way to always replace the least of the three numbers
+  const nums = [array[0] || 1, array[1] || 1, array[2] || 1];
+  for (let i = 3; i < array.length; i++) {
+    // Need a way to track where the min value is so we can replace that value only
+    // with a higher one
+    const pxOfMin = findMin(nums);
+    if (array[i] > nums[pxOfMin]) nums[pxOfMin] = array[i];
+  }
+
+  // Helper
+  // Get the position of the lowest value
+  function findMin(values) {
+    let min = values[0], pxOfMin = 0;
+    for (let i = 1; i < 3; i++) {
+      if (values[i] < min) {
+        min = values[i];
+        pxOfMin = i;
+      }
+    }
+    return pxOfMin;
+  }
+
+  // Multiply the three numbers together
+  return nums.reduce((a, b) => a * b);
 }
-
 
 module.exports = highestProduct;
