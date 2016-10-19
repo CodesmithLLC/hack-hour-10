4b3f800a3c17fb80ce266e1ecbb09d2a046984f9
Sooeung Lee <sooeung2@gmail.com>
2016-10-19 09:52:10 -0700

    highest-product.js

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,25 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) throw new Error();
+  const sorted = array.sort((a,b) => a < b);
+  let result = 1;
+  let resWithNeg = 1;
+  let neg = 0;
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] < 0) {
+      neg++;
+    }
+  }
+  if (neg >= 2) {
+    resWithNeg *= sorted[sorted.length - 1];
+    resWithNeg *= sorted[sorted.length - 2];
+    resWithNeg *= sorted[0];
+  }
+  for (let i = 0; i < 3; i++) {
+    result *= sorted[i];
+  }
+  return Math.max(result, resWithNeg);
 }
-
 
 module.exports = highestProduct;
--- challenges/match-word.js
+++ challenges/match-word.js
@@ -3,12 +3,24 @@
 // matchWord('__END_DNE-----');  -> true
 // matchWord('__ENDDNE__');  -> false       (not separated by a space)
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
-// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
+// matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ]
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
 // matchWord('');  -> true
 
 function matchWord(str) {
-
+	var keywords = str.match(/[a-zA-Z]+/g) || [];
+	var wordStack = [];
+	for (var i = 0; i < keywords.length; i++) {
+		if (
+			keywords[i].split('').reverse().join('').toLowerCase() ===
+			(wordStack[0] || '').toLowerCase()
+		) {
+			wordStack.shift();
+		} else {
+			wordStack.unshift(keywords[i]);
+		}
+	}
+	return !wordStack.length;
 }
 
 module.exports = matchWord;
