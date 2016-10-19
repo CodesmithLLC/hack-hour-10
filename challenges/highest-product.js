88938829ba783fa14fae2425342b747511311dc5
Martin-Ting <martinhting@gmail.com>
2016-10-19 09:40:51 -0700

    completed highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,27 +4,25 @@
 "use strict";
 
 function highestProduct(array) {
-  if(!array){
-    return 0;
-  }
-  if(array.length <= 3){
-    return array.reduce((a,b) => {return a*b}, 1);
-  }
+  if (!array) return 0;
+  if (array.length <= 3) return array.reduce((a,b) => {return a*b}, 1);
 
   let products = {};
+  highestProduct(array, 1, 0);
+  let productValues = Object.keys(products).map((element) => {return parseInt(element);});
+
+  return Math.max.apply(null, productValues);
+
   function highestProduct(array, runningTotal, depth){
-    if(depth === 3){
-      products[runningTotal] = stack;
+    if (depth === 3){
+      products[runningTotal] = 0;
     }
-    for(let i = 0; i < array.length; ++i){
+    for (let i = 0; i < array.length; ++i){
       let newArr = array.slice();
       let currValue = newArr.splice(i,1);
       highestProduct(newArr, currValue * runningTotal, depth + 1);
     }
   }
-  highestProduct(array, 1, 0);
-  let productValues = Object.keys(products).map((element) => {return parseInt(element);});
-  return Math.max.apple(null, productValues);
 }
 
 module.exports = highestProduct;
