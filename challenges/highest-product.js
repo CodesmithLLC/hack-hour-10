8d95041701e1669ea57abf6117b26b95a5f8a0fe
Nick Nelson <ntylernelson@gmail.com>
2016-10-19 09:35:18 -0700

    highest subset

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,12 @@
  */
 
 function highestProduct(array) {
-
+  let negArr = array.sort().slice(0, 2);
+  let posArr = array.sort((a, b) => { return b - a }).slice(0, 3);
+  if (negArr.reduce((a, c) => a * c) > posArr.slice(0, 2).reduce((a, c) => a * c)) {
+    return negArr.concat(posArr[2]).reduce((a, c) => a * c);
+  }
+  else return posArr.reduce((acc, curr) => { return acc * curr });
 }
 
 
