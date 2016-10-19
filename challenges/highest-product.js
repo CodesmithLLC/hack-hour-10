32af8f0acb66aaab6d9969e084edf89c7269dbb3
Eviscerare <edward.h.guo@gmail.com>
2016-10-19 09:31:11 -0700

    finished highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -1,10 +1,10 @@
-/**
- * Given an array of integers, find the highest product you can get from three of the integers.
- */
-
 function highestProduct(array) {
-
+  const positives = array.filter(x => x > 0).sort();
+  const negatives = array.filter(x => x < 0).sort();
+  let x, y, z;
+  if (negatives.length >= 2)
+    negatives.forEach(x => positives.push(Math.abs(x)));
+  return positives.pop() * positives.pop() * positives.pop();
 }
-
 
 module.exports = highestProduct;
