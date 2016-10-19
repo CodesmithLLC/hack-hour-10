70df7e6cda620c0b04405a070b0bacdaf2b626f4
Andres Olivero <andres.eolivero@gmail.com>
2016-10-19 09:42:24 -0700

    hack hour wednesday week 4

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,11 @@
  */
 
 function highestProduct(array) {
-
+  array = array.sort((a, b) => {return a - b;});
+  
+  let firstTwo = array[0] * array[1] * array[array.length-1];
+  let lastThree = array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
+  return firstTwo >= lastThree ? firstTwo : lastThree;
 }
-
 
 module.exports = highestProduct;
