ef6d8c368122d93d76c9c7660ee2e62981d06cc7
Will Galebach <willbach@gmail.com>
2016-10-19 09:33:08 -0700

    finished highest product using helper function

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,16 @@
  */
 
 function highestProduct(array) {
-
+  var highest = 0;
+  function findHigh(arr, count, high) {
+    highest = high > highest? high: highest;
+    if(count === 3 || arr[0] === undefined) {
+      return false;
+    }
+    return findHigh(arr.slice(1), count, high) || findHigh(arr.slice(1), count + 1, arr[0] * high);
+  }
+  findHigh(array, 0, 1);
+  return array[0] === undefined ? 0: highest;
 }
-
 
 module.exports = highestProduct;
