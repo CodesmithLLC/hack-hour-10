677ffa8919562a3528554b20f31065336d5acf26
Hoon Choi <vhchoi@gmail.com>
2016-10-19 09:51:43 -0700

    HH 10/19 submission

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,16 +3,17 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
-  if (array.length < 3) return 0;
-  else {
-    let ansArr = array.sort(function(a, b) {
-      return b - a;
-    });
-    console.log(ansArr);
-    return ansArr[0] * ansArr[1] * ansArr[2];
-  }
-}
+ function highestProduct(array) {
+   if (array.length < 3) return 0;
+   else {
+     let ansArr = [];
+     while (ansArr.length < 3) {
+       ansArr.push(Math.max.apply(null, array));
+       array.splice(array.indexOf(Math.max.apply(null, array)), 1);
+     }
+     return ansArr[0] * ansArr[1] * ansArr[2];
+   }
+ }
 
 
 module.exports = highestProduct;
