e34308b95aa5ca5669b121f607d4aabedf401aa3
ryanbas21 <ryanbas21@gmail.com>
2016-10-19 09:27:35 -0700

    hack hour check

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,10 @@
  */
 
 function highestProduct(array) {
-
+  if (array.length < 3) return 0;
+  const sorted = array.sort((a, b) => a - b);
+  const newArr = [sorted[sorted.length - 1], sorted[sorted.length - 2], sorted[sorted.length - 3]];
+  return newArr.reduce((prev, curr) => prev * curr);
 }
 
 
