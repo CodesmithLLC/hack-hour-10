d7a4acdf025437637b332ee02ad8fb52a9d43209
Nick Nelson <ntylernelson@gmail.com>
2016-10-20 09:51:53 -0700

    Linked Lists

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,12 +4,10 @@
 
 function highestProduct(array) {
 	if(!array || array.length < 3) return 0;
-	let negArr = array.sort((a,b) => {return a-b}).slice(0,3);
+	let negArr = array.sort((a,b) => {return a-b}).slice(0,2);
 	let posArr = array.sort((a,b) => {return b-a}).slice(0,3);
-	if(negArr[0]*negArr[1] > posArr[1]*posArr[2] ){
-		let a = negArr[0]*negArr[1]*posArr[0];
-		if(a > 0) return a;
-	}
+	let mixArr = negArr[0]*negArr[1]*posArr[0];
+	if(negArr[0]*negArr[1] > posArr[1]*posArr[2] && mixArr > 0) return mixArr;
 	return posArr.reduce((a,c) => {return a*c});
 }
 
