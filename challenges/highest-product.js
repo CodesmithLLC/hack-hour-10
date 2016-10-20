e74d7507e3d6e465862f8d2e1a23ae16b72a0e1f
Ahmad K <lance@codemosey.net>
2016-10-20 09:59:05 -0700

    implements zip-linked-lists.js

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -7,19 +7,40 @@ function productOfThree(a, b = 1, c = 1) {
 }
 
 function highestProduct(array) {
-// validate input. If there are less than three elements,
-// simply return their product
-  if (!Array.isArray(array)) return undefined;
-  if (array.length < 3) return productOfThree(...array);
+// validate input.
+  if (!Array.isArray(array)) return 0;
+  if (array.length < 3) return 0;
 
   const highestValArr = [];
+  const lowestValArr = [];
+  let reallyLowVal;
+  let reallyreallyLowVal;
+
   for (let i = 0; i < 3; i++) {
     const highestVal = Math.max(...array);
     const highestIdx = array.indexOf(highestVal);
+    const lowestVal = Math.min(...array);
+    const lowestValIdx =  array.indexOf(lowestVal);
+    array = array.slice(0, lowestValIdx).concat(array.slice(lowestValIdx + 1, array.length));
     array = array.slice(0, highestIdx).concat(array.slice(highestIdx + 1, array.length));
     highestValArr.push(highestVal);
+    lowestValArr.push(lowestVal);
   }
-  return productOfThree(...highestValArr);
+  if (Math.min(...lowestValArr) < 0) {
+    reallyLowVal = Math.min(...lowestValArr);
+    const lowestValIdx =  array.indexOf(lowestVal);
+    lowestValArr = lowestValArr.slice(0, lowestValIdx).concat(lowestValArr.slice(lowestValIdx + 1, lowestValArr.length));
+  }
+  if (Math.min(...lowestValArr) < 0) {
+    reallyreallyLowVal = Math.min(...lowestValArr);
+    const lowestValIdx =  array.indexOf(lowestVal);
+    lowestValArr = lowestValArr.slice(0, lowestValIdx).concat(lowestValArr.slice(lowestValIdx + 1, lowestValArr.length));
+  }
+  if ((reallyreallyLowVal * reallyLowVal) > (highestValArr[0] * highestValArr[1])
+    || (reallyreallyLowVal * reallyLowVal) > (highestValArr[0] * highestValArr[2])
+    || (reallyreallyLowVal * reallyLowVal) > (highestValArr[1] * highestValArr[2])) {
+      return productOfThree(reallyLowVal, reallyreallyLowVal, highestValArr[0]);
+  } else return productOfThree(...highestValArr);
 }
 
 
