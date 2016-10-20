bd957330c7756cbe39efd99464eae16eb9db234c
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-10-20 09:59:38 -0700

    Thai's zip-linked-lists.js

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,22 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  const head = l1;
+  let curr = l1;
+  let next1 = l1.next
+  let next2 = l2.next;
+  if (l2) curr.next = l2;
+  if (curr.next) curr = curr.next;
+
+  while (curr) {
+    if (next1) curr.next = next1;
+    if (curr.next) curr = curr.next; else break;
+    next1 = next1.next;
+    if (next2) curr.next = next2;
+    if (curr.next) curr = curr.next; else break;
+    next2 = next2.next;
+  }
+  return head;
 };
 
 module.exports = {Node: Node, zip: zip};
