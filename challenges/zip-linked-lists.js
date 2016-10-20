9fa99a2e6e450643b7fd76e853219a6dfaed6a69
David Wilson <w.dave.w@gmail.com>
2016-10-20 09:58:46 -0700

    zip linked lists

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -5,12 +5,55 @@
  * BONUS: Do this in place
  */
 
+// n - n - n
+// a - a - a
+// n - a - n - a - n - a
+
+
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
 function zip(l1, l2) {
+
+  if (!(l1 && l2)) {
+    return l1? l1 : l2;
+  }
+  let counter = 0;
+  let currentL1Node = l1;
+  let currentL2Node = l2;
+  let nodeToZip = l2;
+  let currentNode = currentL1Node;
+  let startNode = l1;
+  while (currentL1Node && currentL2Node) {
+    // alternate between ll1 and ll2
+    let nodeToZip = (counter % 2 === 1) ? currentL1Node : currentL2Node;
+
+    // zip the next node from the other list and increment counter
+    nextCurrentNode = currentNode.next;
+    currentNode.next = nodeToZip;
+
+    // move pointer to the zipped node
+    currentNode = nodeToZip;
+
+    // update head pointers
+    if (counter % 2 === 0) {
+      currentL1Node = nextCurrentNode;
+    }
+    else {
+      currentL2Node = nextCurrentNode;
+    }
+    counter += 1;
+
+  }
+  // add whatever is leftover
+  let leftover = currentL1Node? currentL1Node : currentL2Node;
+  currentNode.next = leftover.next;
+  return l1
+
 };
 
-module.exports = {Node: Node, zip: zip};
+
+
+module.exports = { Node: Node, zip: zip };
