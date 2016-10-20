1b463f91555c37e4afff23638b3a108fec1d065b
Sooeung Lee <sooeung2@gmail.com>
2016-10-20 09:55:45 -0700

    zip-ll

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,46 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (l1 === null) return l2;
+  if (l2 === null) return l1;
+
+  let head = l1;
+  let temp = l1;
+  l1 = l1.next;
+
+  while (l1 && l2) {
+    temp.next = l2;
+    l2 = l2.next;
+    temp = temp.next;
+
+    temp.next = l1;
+    l1 = l1.next;
+    temp = temp.next;
+  }
+
+  temp.next = l2 ? l2 : l1;
+  return head;
+
 };
 
+
 module.exports = {Node: Node, zip: zip};
+
+var a = new Node('A');
+var b = new Node('B');
+var c = new Node('C');
+var d = new Node('D');
+a.next = b;
+b.next = c;
+c.next = d;
+
+
+var A = new Node('a');
+var B = new Node('b');
+var C = new Node('c');
+var D = new Node('d');
+A.next = B;
+B.next = C;
+C.next = D;
+
+console.log(zip(a, A));
