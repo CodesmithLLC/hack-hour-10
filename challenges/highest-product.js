f835871d4b3fa6d6ffa0f9aa59ea9690331a6c81
ryanbas21 <ryanbas21@gmail.com>
2016-10-19 21:43:32 -0700

    work

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,23 +2,16 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
- function highestProduct(array) {
+ function highestProduct(array, count = 3, multiply = 1) {
+
    if (!array) return 0;
    if (array.length < 3) return 0;
-   const sorted = array.sort((a, b) => a - b);
-   const newArr = [sorted[sorted.length - 1], sorted[sorted.length - 2], sorted[sorted.length - 3]];
-   const negCheck = [sorted[0], sorted[1]];
-   const posCheck = [sorted[sorted.length - 1], sorted[sorted.length - 2]];
-   if (sorted[sorted.length-1] * sorted[sorted.length-2] > newArr.reduce(prev,curr) => prev*curr){
-   		negCheck.reduce( (prev,curr) => prev* curr) * newArr[sorted.length-1];
-   }
-   if (sorted[0] <= 0 && sorted[sorted.length - 1] <= 0) {
-     return sorted[sorted.length-1] * sorted[sorted.length-2] * sorted[sorted.length-3];
-   }
-   if (negCheck.reduce((prev, curr) => prev * curr) > posCheck.reduce((prev, curr) => prev * curr)) {
-     return negCheck.reduce((prev, curr) => prev * curr) * newArr[0];
-   }
-   return newArr.reduce((prev, curr) => prev * curr);
+   if (count === 0) return multiply;
+   return Math.max(
+    highestProduct(array.slice(1)), count - 1, multiply * array[0],
+    highestProduct(array.slice(1), count, multiply)
+);
+
  }
 
 
