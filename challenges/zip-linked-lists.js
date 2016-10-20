57b161d2dfebad68528d0e98d20528da7a946cfb
Cheng Ly <chengsieuly@gmail.com>
2016-10-20 10:40:11 -0700

    Finish HH W3D4

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,7 +10,30 @@ function Node(val) {
   this.next = null;
 }
 
-function zip(l1, l2) {
+function zip(n1, n2) {
+  // case where first argument does not exist
+  // We can simply set n1 to n2;
+  if (!n1) return (n1 = n2);
+
+  // Case where there is no more nodes on list 1 to zip
+  // so we will simply return
+  if (!n2) return;
+
+  const t1 = n1.next;
+  const t2 = n2.next;
+
+  // Case where there is no more nodes on list 2 to zip
+  // so we will simply just take whatever we have and attach it
+  // to the remaining nodes on list 1
+  if (!t1) return (n1.next = n2);
+
+  const t3 = t1.next;
+  n1.next = n2;
+  n2.next = t1;
+  t1.next = t2;
+
+  if (t1 && !t2) zip(t1, t3);
+  else zip(t2, t3);
 };
 
-module.exports = {Node: Node, zip: zip};
+module.exports = { Node: Node, zip: zip };
