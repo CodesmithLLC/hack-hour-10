b0ac4830333d85e22020cfe5995586e8aae5bcdf
sahilnayak <snayak.researcher@gmail.com>
2016-10-21 09:40:04 -0700

    Adding Stack Problem

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,30 @@
 
 function Stack() {
   // body...
+  this.stack = {};
+  this.max = 0;
+  this.index = 0;
+
+  this.push = function(value){
+
+    if(value > this.max) this.max = value;
+    this.index++;
+    this.stack[this.index] = value;
+  }
+
+  this.pop = function() {
+    if(this.index === 0) return this.stack;
+    else { 
+      let value = this.stack[this.index-1];
+      this.index--;
+      return value;
+    }
+  }
+
+  this.getMax = function() {
+    return this.max;
+    
+  }
 }
 
 module.exports = Stack;
