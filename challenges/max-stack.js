75884ff9c94916f75ab08933dd71bbcd3c921524
Dean Edwards <dmedwards4@gmail.com>
2016-10-21 10:00:42 -0700

    max-stack challenge

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,72 @@
  */
 
 function Stack() {
-  // body...
+  this.index = 0;
+  this.storage = {};
 }
 
+Stack.prototype.push = function (value) {
+  this.storage[this.index] = value;
+  this.index++;
+}
+
+Stack.prototype.pop = function() {
+  let value = this.storage[this.index- 1]; 
+  delete this.storage[this.index - 1];
+  this.index--;
+  return value;
+}
+
+Stack.prototype.getMax = function () {
+  let max = -Infinity;
+  for (let key in this.storage) {
+    if (this.storage[key] >= max) {
+      max = this.storage[key];
+    }
+  }
+  return max;
+}
+
+// let test = new Stack();
+// test.push(1);
+// test.push(2);
+// test.push(3);
+// test.push(4);
+// test.pop(4);
+// let max = test.getMax();
+// console.log(test, max);
+
+
 module.exports = Stack;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Stack() {
+  this.storage = {};
+  this.index = 0;
+}
+
+Stack.prototype.push = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Stack.prototype.pop = function() {
+    let value = this.storage[this.index - 1]; 
+    delete this.storage[this.index - 1];
+    this.index--;
+    return value;
+};
