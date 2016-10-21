84af5622f279b037150fed3a579ac8d5e04e5e1f
ryanbas21 <ryanbas21@gmail.com>
2016-10-20 22:52:53 -0700

    submitting

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -1,10 +1,4 @@
-/* Merge two linked lists so that their nodes alternate. Let the first node of the zipped list be
- * the first node of the first argument, if it exists.
- * Implement the linked list using only a Node class. No need for a wrapper LinkedList class
- *
- * BONUS: Do this in place
- */
-
+"use strict"
 function Node(val) {
   this.value = val;
   this.next = null;
@@ -13,27 +7,48 @@ function Node(val) {
 function zip(l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
+
   let head = l1;
   let temp1;
   let temp2;
-  while (!l1.next || !l2.next){
-    temp = l1.next;
+  while (!l1.next || l2.next){
+    temp1 = l1.next;
     l1.next = l2;
     temp2 = l2.next;
     l2.next = temp1;
     l1 = temp1;
     l2 = temp2;
   }
-  if (!l1.next && !l2.next) l1.next = l2;
-  if (!l1.next){
-    l1.next = l2;
-    l2.next = temp1;
+  if (!l1.next && !l2.next){
+  	l1.next = l2;
   }
-  if (!l2.next){
+  else if (!l1.next){
+    temp2 = l2.next;
     l2.next = l1;
     l1.next = temp2;
   }
+  else if (!l2.next){
+    temp1 = l1.next;
+    l1.next = l2;
+    l2.next = temp1;
+  }
+  print(head);
   return head;
 }
+function print (l1) {
+  let temp = l1;
+  while (temp){
+    temp = temp.next
+  }
+}
+let newNode1 = new Node('a');
+ newNode1.next = new Node('b');
+ newNode1.next.next = new Node('c');
+ newNode1.next.next.next = new Node('d');
+let newNode2 = new Node('1');
+ newNode2.next = new Node('2');
+ newNode2.next.next = new Node('3');
 
-module.exports = {Node: Node, zip: zip};
+
+ zip(newNode1,newNode2);
+ module.exports = {Node: Node, zip: zip};
