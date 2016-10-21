b48f25f43b39be9c97d05426991ae3713d5788af
Will Galebach <willbach@gmail.com>
2016-10-21 09:43:35 -0700

    finished max stack hack hour without bonus

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,25 @@
  */
 
 function Stack() {
-  // body...
+  this.length = 0;
+  this.storage = {};
+  this.push = (item) => {
+    this.storage[this.length++] = item;
+    return this.length;
+  }
+  this.pop = () => {
+    let value = this.storage[(this.length - 1)];
+    delete this.storage[(this.length - 1)];
+    return value;
+  }
+  this.getMax = () => {
+    let largest = -Infinity;
+    for(let index in this.storage) {
+      largest = this.storage[index] > largest ? this.storage[index] : largest;
+    }
+    return largest;
+  };
 }
+
 
 module.exports = Stack;
