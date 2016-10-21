c5bb64ed1f8e3b5a50cc505ac7bebcde0b2baed8
ryanbas21 <ryanbas21@gmail.com>
2016-10-20 23:22:14 -0700

    pray

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,23 +11,26 @@ function zip(l1, l2) {
   let head = l1;
   let temp1;
   let temp2;
-  while (!l1.next || l2.next){
+  while (!l1.next && !l2.next){
     temp1 = l1.next;
     l1.next = l2;
+    console.log(l1.next.value +'setting l1.next = l2')
     temp2 = l2.next;
     l2.next = temp1;
     l1 = temp1;
     l2 = temp2;
+    console.log(l1.value, "end" ,l2.value);
   }
-  if (!l1.next && !l2.next){
+  if (!l1.next || !l2.next){
   	l1.next = l2;
   }
   else if (!l1.next){
-    temp2 = l2.next;
-    l2.next = l1;
-    l1.next = temp2;
+    console.log('that');
+    l1.next = l2;
   }
+  //works
   else if (!l2.next){
+    console.log('this one')
     temp1 = l1.next;
     l1.next = l2;
     l2.next = temp1;
@@ -38,17 +41,18 @@ function zip(l1, l2) {
 function print (l1) {
   let temp = l1;
   while (temp){
+    console.log( temp.value, "=>" );
     temp = temp.next
   }
 }
 let newNode1 = new Node('a');
  newNode1.next = new Node('b');
  newNode1.next.next = new Node('c');
- newNode1.next.next.next = new Node('d');
+
 let newNode2 = new Node('1');
  newNode2.next = new Node('2');
  newNode2.next.next = new Node('3');
-
+newNode2.next.next.next = new Node('d');
 
  zip(newNode1,newNode2);
  module.exports = {Node: Node, zip: zip};
