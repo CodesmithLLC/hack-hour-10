ecf2f1808e97a2c86587614dfa17aec69a3e4f78
ryanbas21 <ryanbas21@gmail.com>
2016-10-20 22:31:47 -0700

    submitting

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -13,15 +13,27 @@ function Node(val) {
 function zip(l1, l2) {
   if (!l1) return l2;
   if (!l2) return l1;
+  if
   let head = l1;
-  let temp;
+  let temp1;
+  let temp2;
   while (!l1.next || !l2.next){
     temp = l1.next;
     l1.next = l2;
-    l2.next = temp;
-    l1 = temp;
+    temp2 = l2.next;
+    l2.next = temp1;
+    l1 = temp1;
+    l2 = temp2;
   }
-
+  if (!l1.next && !l2.next) l1.next = l2;
+  if (!l1.next){
+    l1.next = l2;
+    l2.next = temp1;
+  }
+  if (!l2.next){
+    l2.next = l1;
+    l1.next = temp2;
+  }
   return head;
 }
 
