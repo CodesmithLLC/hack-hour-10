7058c5c95a96893661c6e20e9c0f447be82745b1
travishuff <huff.travis@gmail.com>
2016-10-24 09:17:20 -0700

    ll

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,32 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+  if (!head || !head.next) return head;
 
+  let previous = null;
+  let current = head;
+  let next;
+
+  while (current) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  head = previous;
+  return head;
 }
+
+var bird = { value: 'DoDo' };
+var cat = { value: 'felix' };
+var dog = { value: 'Scooby Doo' };
+var duck = { value: 'Howard' };
+bird.next = cat;
+cat.next = dog;
+dog.next = duck;
+
+console.log(reverseLinkedList(bird));
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
