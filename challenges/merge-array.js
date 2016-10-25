4e3a576e0205cfc8030b55688c3fdd6a66edbadd
Eviscerare <edward.h.guo@gmail.com>
2016-10-25 09:32:33 -0700

    finished merge array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -1,20 +1,21 @@
-/**
- * We have our lists of orders sorted numerically already, in arrays.
- * Write a functin to merge our arrays of orders into one sorted array.
- * These may be of different lengths.
- *
- * var my_array = [3,4,6,10,11,15,21];
- * var another_array = [1,5,8,12,14,19];
- *
- * mergeArrays(my_array, another_array); -> [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19, 21]
- * 
- * BONUS: 
- * Complete in O(n) time and O(n) space 
- *
- */
-
 function mergeArrays(arr1, arr2) {
-
+  if (!arr1 && !arr2) return undefined;
+  if (!arr1) return arr2;
+  if (!arr2) return arr1;
+  let sorted = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] < arr2[0]) {
+      sorted.push(arr1.shift());
+    } else {
+      sorted.push(arr2.shift());
+    }
+  }
+  return sorted.concat(arr1).concat(arr2);
 }
 
 module.exports = mergeArrays;
+
+// var my_array = [3, 4, 6, 10, 11, 15, 21];
+// var another_array = [1, 5, 8, 12, 14, 19];
+
+// console.log(mergeArrays(my_array, another_array));
