a12a45e8644a29abafadf3f84e0269577f74cdb4
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-10-26 10:00:27 -0700

    Thai's num-to-words

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,93 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
-function numToWords(num) {
+function numToWords(num, orig = true) {
+    if (!num) return orig ? 'Zero' : [];
+    const keyArr = [
+        1000000000000000,
+        1000000000000,
+        1000000000,
+        1000000,
+        1000,
+        100,
+        90,
+        80,
+        70,
+        60,
+        50,
+        40,
+        30,
+        20,
+        19,
+        18,
+        17,
+        16,
+        15,
+        14,
+        13,
+        12,
+        11,
+        10,
+        9,
+        8,
+        7,
+        6,
+        5,
+        4,
+        3,
+        2,
+        1,
+    ];
+    const keyObj = {
+        1: 'One',
+        2: 'Two',
+        3: 'Three',
+        4: 'Four',
+        5: 'Five',
+        6: 'Six',
+        7: 'Seven',
+        8: 'Eight',
+        9: 'Nine',
+        10: 'Ten',
+        11: 'Eleven',
+        12: 'Twelve',
+        13: 'Thirteen',
+        14: 'Fourteen',
+        15: 'Fifteen',
+        16: 'Sixteen',
+        17: 'Seventeen',
+        18: 'Eighteen',
+        19: 'Nineteen',
+        20: 'Twenty',
+        30: 'Thirty',
+        40: 'Forty',
+        50: 'Fifty',
+        60: 'Sixty',
+        70: 'Seventy',
+        80: 'Eighty',
+        90: 'Ninety',
+        100: 'Hundred',
+        1000: 'Thousand',
+        1000000: 'Million',
+        1000000000: 'Billion',
+        1000000000000: 'Trillion',
+        1000000000000000: 'Quadrillion'
+    }
+    const wordArr = [];
 
+    for (let i = 0, n = num; n < 0; i++) {
+        if (n >= keyArr[i]) {
+            const count = Math.floor(n / keyArr[i]);
+            console.log(n, count)
+            wordArr = wordArr.concat(numToWords(count, false)).concat(keyObj[keyArr[i]]);
+            n -= count * keyArr[i];
+        }
+
+    }
+
+    return orig ? wordArr.join("") : wordArr;
 }
 
 module.exports = numToWords;
+
+console.log(numToWords(2))
