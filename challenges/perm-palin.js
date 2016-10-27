23570ccbea674f70492d524da8de4899e821f341
ryanbas21 <ryanbas21@gmail.com>
2016-10-27 13:55:45 -0700

    testingthis solution

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,33 +9,21 @@
  *
  */
 function permPalin(str) {
-    const original = str;
-    if (str.split('').reverse().join() === str) return true;
+    const letterCounts = {};
     if (str.length === 1) return true;
-    const stringArr = str.split('');
-
-
-
-
-    function permutations(string, count, str1) {
-        console.log(str1);
-        let newString = string[string.length - 1].toString() + string.slice(0, string.length - 1)
-        newString = newString.replace(/,/g,"");
-        console.log(newString, str);
-        if (newString === str1){
-            console.log('here');
-            if (count === 1){
-                return false;
-            }
-            count++;
-         }
-        if (newString.split('').reverse().join('') === string){
-            return true;
-        }
-        permutations(newString,count,str1);
+    if (!str) return false;
+    let letter;
+    let palindromeSum = 0;
+    for (let i = 0; i < str.length; i++) {
+        letter = str[i];
+        letterCounts[letter] = letterCounts[letter] || 0;
+        letterCounts[letter]++;
     }
-
-    return permutations(stringArr, 0, original)
-}
-permPalin('cbac');
+    for (let letterCount in letterCounts) {
+        palindromeSum += letterCounts[letterCount] % 2;
+    }
+    console.log(palindromeSum);
+    return palindromeSum < 1;
+    }
+permPalin('cbaba');
 module.exports = permPalin;
