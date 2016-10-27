86507c95a04b7e6ab9a853ec4a9c514138ca0232
ryanbas21 <ryanbas21@gmail.com>
2016-10-27 11:23:49 -0700

    finished

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -8,9 +8,34 @@
  * 	- permPalin('a') => true
  *
  */
-
 function permPalin(str) {
-	
-}
+    const original = str;
+    if (str.split('').reverse().join() === str) return true;
+    if (str.length === 1) return true;
+    const stringArr = str.split('');
 
+
+
+
+    function permutations(string, count, str1) {
+        console.log(str1);
+        let newString = string[string.length - 1].toString() + string.slice(0, string.length - 1)
+        newString = newString.replace(/,/g,"");
+        console.log(newString, str);
+        if (newString === str1){
+            console.log('here');
+            if (count === 1){
+                return false;
+            }
+            count++;
+         }
+        if (newString.split('').reverse().join('') === string){
+            return true;
+        }
+        permutations(newString,count,str1);
+    }
+
+    return permutations(stringArr, 0, original)
+}
+permPalin('cbac');
 module.exports = permPalin;
