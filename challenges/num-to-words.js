799c7cf0edeba61d284ebf70231011576695b614
mlampedx <mlampesd@gmail.com>
2016-10-27 09:51:17 -0700

    perm-palin.js complete

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,68 @@
  */
 
 function numToWords(num) {
+let str = '';
+let numStr = String(num);
 
+// Add 0s to front of numStr so it is divisible by 3 - function convertToString(num)
+// Create arrays of length three
+// Add millions, thousands etc.
+// Add 0-100
+
+let arr = [];
+let temp = [];
+let nums = {
+  '0': 'Zero',
+  '1': 'One',
+  '2': 'Two',
+  '3': 'Three',
+  '4': 'Four',
+  '5': 'Five',
+  '6': 'Six',
+  '7': 'Seven',
+  '8': 'Eight',
+  '9': 'Nine',
+  '10': 'Ten',
+  '11': 'Eleven',
+  '12': 'Twelve',
+  '13': 'Thirteen',
+  '14': 'Fourteen',
+  '15': 'Fifteen',
+  '16': 'Sixteen',
+  '17': 'Seventeen',
+  '18': 'Eighteen',
+  '19': 'Nineteen',
+  '20': 'Twenty',
+  '30': 'Thirty',
+  '40': 'Forty',
+  '50': 'Fifty',
+  '60': 'Sixty',
+  '70': 'Seventy',
+  '80': 'Eighty',
+  '90': 'Ninety'
+};
+
+for (let i = 1; i < numStr.length; i++) {
+  temp.push(numStr[i]);
+  if (i % 3 === 0) {
+  	arr.push(temp);
+  	temp = [];
+  }
 }
 
+let len = arr.length;
+
+console.log(arr)
+
+if (arr.length === 1) {
+  if (arr[len - 1].length === 3) {
+    str += nums[arr[len - 1][0]] + 'Hundred';
+  }
+  if (arr[len - 1].length >= 2 && arr[len - 1][])
+}
+
+console.log(str)
+}
+
+console.log(numToWords(200))
 module.exports = numToWords;
