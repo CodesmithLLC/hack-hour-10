3a6de5a53cfd1255aaafd5cd62155d8a7a82aef0
Michael Sotkin <michael.sotkin@gmail.com>
2016-10-27 09:25:05 -0700

    finished hack hour

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,53 @@
  */
 
 function numToWords(num) {
+    const table = {
+		Ninety: 90,
+		Eighty: 80,
+		Seventy: 70,
+		Sixty: 60,
+		Fifty: 50,
+		Forty: 40,
+		Thirty: 30,
+		Twenty: 20,
+		Nineteen: 19,
+		Eighteen: 18,
+		Seventeen: 17,
+		Sixteen: 16,
+		Fifteen: 15,
+		Fourteen: 14,
+		Thirteen: 13,
+		Twelve: 12,
+		Eleven: 11,
+		Ten: 10,
+		Nine: 9,
+		Eight: 8,
+		Seven: 7,
+		Six: 6,
+		Five: 5,
+		Four: 4,
+		Three: 3,
+		Two: 2,
+		One: 1
+	};
+	result = '';
+	
+	if (num === 0) return 'Zero';
 
+	if (num.toString().length === 3) {
+		let temp = Math.floor(num / 100);
+		helper(temp);
+		result+='Hundred';
+	}
+function helper(num) {
+	for (let prop in table) {
+		while (num >= table[prop]) {
+			result += prop;
+			num -= table[prop];
+		}
+	}
+}
+	return result;
 }
 
 module.exports = numToWords;
