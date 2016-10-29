2a37c4e6d4cdf9742ef956d901e0617113f87b37
Will Galebach <willbach@gmail.com>
2016-10-29 09:39:10 -0700

    finished stacking dat queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,41 @@
 
 
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
+  this.push = (item) => {
+    this.storage[this.index++] = item;
+    return this.index;
+  }
+  this.pop = () => {
+    let value = this.storage[--this.index];
+    delete this.storage[this.index];
+    return value;
+  }
 }
-
 
 /**
 * Queue Class
 */
 
-
 function Queue() {
+  this.iniStorage = new Stack();
+  this.finStorage = new Stack();
+  this.push = (item) => this.iniStorage.push(item);
+
+  this.shift = () => {
+    while(this.iniStorage.index > 0) {
+      this.finStorage.push(this.iniStorage.pop());
+    }
+    let shifted = this.finStorage.pop();
+    while(this.finStorage.index > 0) {
+      this.iniStorage.push(this.finStorage.pop());
+    }
+    return shifted;
+  }
 
 }
+
+
 
 module.exports = {Stack: Stack, Queue: Queue};
