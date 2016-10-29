8f6b4c2ddc274e5aba405fdcde7da6805cb3fa7e
Andres Olivero <andres.eolivero@gmail.com>
2016-10-29 09:48:43 -0700

    hack hour saturday week 5

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,18 @@
 
 
 function Stack() {
-
+  this.storage = {};
+  this.index = 0;
 }
 
+Stack.prototype.push = (val) => {
+  this.storage[this.index++] = val;
+}
+Stack.prototype.pop = () => {
+  let popped = this.storage[--this.index];
+  delete this.storage[this.index];
+  return popped;
+}
 
 /**
 * Queue Class
@@ -14,7 +23,23 @@ function Stack() {
 
 
 function Queue() {
+  this.storage = new Stack;
+  this.nextDequeue = new Stack;
+}
 
+Queue.prototype.enqueue = (val) => {
+  if (!this.nextDequeue[0]) { this.nextDequeue.push(val) }
+  else {
+    this.storage.push(val);
+  }
+}
+
+Queue.prototype.dequeue = () => {
+  let dequeued = this.nextDequeue.pop();
+  this.nextDequeue[0] = this.storage[0];
+  delete this.storage[0];
+  this.storage.index -= 1;
+  return dequeued;
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
