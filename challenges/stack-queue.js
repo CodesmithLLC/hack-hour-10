30e75ad6d469eca57abc78a7846d304ba709146d
Eviscerare <edward.h.guo@gmail.com>
2016-10-29 09:50:19 -0700

    cleaned up

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -1,16 +1,8 @@
-/**
- * Create a stack.Then create a queue using two stacks.
- */
-
 function Stack() {
   this.stack = [];
   this.push = value => this.stack.push(value);
   this.pop = () => this.stack.pop()
 }
-
-/**
-* Queue Class
-*/
 
 function Queue() {
   this.a = new Stack();
@@ -27,18 +19,3 @@ function Queue() {
 }
 
 module.exports = { Stack: Stack, Queue: Queue };
-
-let queue = new Queue();
-
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-queue.enqueue(4);
-queue.enqueue(5);
-queue.enqueue(6);
-
-console.log(queue);
-
-queue.dequeue();
-
-console.log(queue);
