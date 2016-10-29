103368fa785bfcf5ab330ab25ba4c908b9716dd6
Nick Nelson <ntylernelson@gmail.com>
2016-10-29 10:00:49 -0700

    10-29-16 Hack Hour

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,17 +4,33 @@
 
 
 function Stack() {
-
+  this.storage = [];
+  this.push = function(input) {
+    return this.storage.push(input);
+  };
+  this.pop = function () {
+    return this.storage.pop();
+  };
 }
 
-
-/**
-* Queue Class
-*/
-
-
 function Queue() {
-
+  this.end = new Stack();
+  this.beginning = new Stack();
+  this.dequeue = function () {
+    while(this.end.length > 1) {
+    this.beginning.push(this.end.pop());
+    }
+    let output = this.end.pop();
+    if( this.beginning.length > 0) {
+    	let swap = this.beginning;
+    	this.beginning = this.end;
+    	this.end = swap;
+    }
+    return output;
+  };
+  this.enqueue = function (ele) {
+    return this.end.push(ele);
+  };
 }
 
 module.exports = {Stack: Stack, Queue: Queue};
