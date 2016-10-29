19ad70e1d3018d7614f9e5aeb3b9d10f10275f9d
Michael Sotkin <michael.sotkin@gmail.com>
2016-10-29 09:45:14 -0700

    finished stack-queue hack hour

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,8 +4,16 @@
 
 
 function Stack() {
-
+	this.storage = [];
 }
+
+Stack.prototype.push = function(value) {
+	this.storage.push(value);
+};
+
+Stack.prototype.pop = function() {
+	this.storage.pop();
+};
 
 
 /**
@@ -14,7 +22,22 @@ function Stack() {
 
 
 function Queue() {
-
+	this.storage = [new Stack(), new Stack()];
 }
+
+Queue.prototype.enqueue = function(value) {
+	this.storage[0].storage.push(value);
+};
+
+Queue.prototype.dequeue = function() {
+	if (this.storage[1].storage.length > 0) {
+		this.storage[1].storage.pop();
+	} else {
+		for (let i = 0; i < this.storage[0].storage.length; i++) {
+			this.storage[1].storage.push(this.storage[0].storage.pop());
+		}
+		this.storage[0].storage.pop();
+	}
+};
 
 module.exports = {Stack: Stack, Queue: Queue};
