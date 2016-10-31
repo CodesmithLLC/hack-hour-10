d0dd593d0405b1ae1d805763d321362c5620b509
Eviscerare <edward.h.guo@gmail.com>
2016-10-31 09:44:17 -0700

    twosum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,13 @@
  */
 
 function twoSum(arr, n) {
-
+  function tS(arr, n, count) {
+    if (count > 2) return false;
+    if (arr[0] === n) return true;
+    if (!arr.length) return false;
+    return tS(arr.slice(1), n, count) || tS(arr.slice(1), n - arr[0], count+1);
+  }
+  return tS(arr, n, 0);
 }
 
 module.exports = twoSum;
