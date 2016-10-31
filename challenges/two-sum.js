ec16c5aa29289a45a73a9b485e2648a4c07d9c44
Andres Olivero <andres.eolivero@gmail.com>
2016-10-31 09:48:15 -0700

    hack hour monday week 6

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,20 @@
  */
 
 function twoSum(arr, n) {
+  if (arr.length < 2) return false;
 
+  // for (let i = 0; i < arr.length - 1; i++){
+  //   for (let j = i + 1; j < arr.length; j++){
+  //     if (arr[i] + arr[j] === n) return true;
+  //   }
+  // }
+  // return false;
+  let last = arr.pop();
+  for (let i = 0; i < arr.length; i++) {
+    let total = arr[i] + last;
+    if (total === n) return true;
+  }
+  return twoSum(arr, n);
 }
 
 module.exports = twoSum;
