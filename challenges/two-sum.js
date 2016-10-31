7b14ec59b8528d7d275cef7b088398e29f29159f
Cheng Ly <chengsieuly@gmail.com>
2016-10-31 09:42:34 -0700

    Finish HH W6D1 two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,21 @@
  */
 
 function twoSum(arr, n) {
+  // Store a cache of differences as we iterate through the array
+  // and subtract it from n
+  // We then need to check if there is a number in our cache that
+  // contains the currently iterated value
+  // If so, then we know there exist a number
 
+  const store = {};
+  let found = false;
+
+  arr.forEach(val => {
+    if (!store[val]) store[n - val] = true;
+    else found = true;
+  });
+
+  return found;
 }
 
 module.exports = twoSum;
