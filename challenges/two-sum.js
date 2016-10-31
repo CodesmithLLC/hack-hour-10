a52ff66b94e590f051743f34c18abcb345c68e2a
Will Galebach <willbach@gmail.com>
2016-10-31 09:37:05 -0700

    finished two sum recursively

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,12 @@
  */
 
 function twoSum(arr, n) {
-
+  function find(num, array, count) {
+    if(num === n && count === 2) return true;
+    if(array[0] === undefined || count > 1) return false;
+    return find(num + array[0], array.slice(1), count + 1) || find(num, array.slice(1), count);
+  }
+  return find(0, arr, 0);
 }
 
 module.exports = twoSum;
