9ddd1004d829e3339534a6f9e857e4ac70359a11
Cheng Ly <chengsieuly@gmail.com>
2016-10-31 09:42:05 -0700

    Finish HH W5D5 perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,19 +9,54 @@
  *
  */
 
-function checkPalin(str) {
-  const reverse = str.split('').reverse().join('');
-  if (str === reverse) return true;
-  return false;
+// This is a helper to create all the possible permutations
+// It plucks out a character and returns the remaining str
+function pluck(str, position) {
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (i === position) continue;
+    newStr += str[i];
+  }
+
+  return newStr;
+}
+
+// Checks if the string is a palindrome in a non-optimized way =)
+function check(str) {
+  const normalizedStr = str.replace(/[^\w]/g, '').toLowerCase();
+  return normalizedStr.split('').reverse().join('') === normalizedStr;
 }
 
 function permPalin(str, concat = '') {
-  // What is a palindrome?
-  // A palindrome is a string that is the same when it is reversed
-  if (str.length < 1) return checkPalin(concat);
-  return permPalin(str.slice(1), concat + str[0]) || str.slice(2);
+  if (str.length < 1 && check(concat)) return true;
+
+  for (let i = 0; i < str.length; i++) {
+    if (permPalin(pluck(str, i), concat + str[i])) return true;
+  }
+
+  return false;
 }
 
-console.log(permPalin('tart'));
-
 module.exports = permPalin;
+
+function permPalin2(str) {
+  const normalizedStr = str.replace(/[^\w]/g, '').toLowerCase();
+
+  const store = {};
+
+  // Count the characters of str and store in an object
+  for (let i = 0; i < normalizedStr.length; i++) {
+    const char = normalizedStr[i];
+    if (!store[char]) store[char] = 0;
+    store[char]++;
+  }
+
+  let oddCount = 0;
+  for (const count in store) {
+    if (store[count] % 2 !== 0) oddCount++;
+    if (oddCount > 1) return false;
+  }
+
+  return true;
+}
