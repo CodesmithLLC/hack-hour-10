45af925e215d8c750e7f0e8a20a1e72092c8db6b
mlampedx <mlampesd@gmail.com>
2016-10-31 09:32:26 -0700

    two-sum.js complete

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,16 @@
  */
 
 function twoSum(arr, n) {
+if (!Array.isArray(arr)) { return false; }
+if (arr.length < 2) { return false; }
+let target = n;
 
+for (let i = 0; i < arr.length; i++) {
+  if (arr.includes(target - arr[i]) && arr.lastIndexOf(target - arr[i]) !== i) {
+    return true;
+  }
+}
+return false;
 }
 
 module.exports = twoSum;
