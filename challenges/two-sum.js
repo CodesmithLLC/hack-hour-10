c6b3221c4134f211ee5ee07e6a1652c82c7852a6
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-10-31 09:44:27 -0700

    brute force method

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,18 @@
  */
 
 function twoSum(arr, n) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] + arr[j] === n && i !== j) return true;
+    }
+  }
 
+  return false;
 }
+
+// const arr1 = [1, 4, 6, 9];
+// const arr2 = [-3, -2, 9, 4];
+// console.log(twoSum(arr1, 8));
+// console.log(twoSum(arr2, 4));
 
 module.exports = twoSum;
