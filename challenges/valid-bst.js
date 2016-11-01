f2b29491311540fdb3f7a8fc1d4291a2f50bf6d3
srei <steferu@gmail.com>
2016-11-01 09:31:20 -0700

    ?

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,7 +3,7 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
+
 
 function BinaryTree(val) {
     this.value = val;
@@ -12,7 +12,12 @@ function BinaryTree(val) {
 }
 
 function validBST(tree) {
+	let rootNode = tree.value;
+	if(tree === null) return true;
+	if(tree.left !== undefined && tree.left.value > rootNode) return false;
+	if(tree.right !== undefined && tree.right.value <= tree.value) return false;
 
+	return validBST(tree.left) && validBST(tree.right);
 }
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
