94f5a4435df9174ce76bee4fad4a75d7d9cc4cd9
Dean Edwards <dmedwards4@gmail.com>
2016-11-01 09:58:45 -0700

    valid-bst hh

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -3,16 +3,52 @@
  *      at any given node, the value of all the nodes in its left tree must be <= its value
  *      at any given node, the value of all the nodes in its right tree must be > its value
  */
- 
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
+  let valid = true;
+  if (tree.value === undefined) return false;
+  
+  function traverse(current) {
+    console.log(current, valid);
+    if (current.value) {
+      if (current.left !== null) {
+        if (current.left.value > current.value) valid = false;
+        traverse(current.left);
+      }
+      if (current.right !== null) {
+        if (current.right.value < current.value) valid = false;
+        traverse(current.right);
+      }
+    }
+    return;
+  }
+  traverse(tree);
 
+  return valid;
 }
+let a = new BinaryTree(20);
+let al = new BinaryTree(10);
+let ar = new BinaryTree(25);
+let bl = new BinaryTree(5);
+let br = new BinaryTree(13);
+let cl = new BinaryTree(22);
+let cr = new BinaryTree(27);
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+a.left = al;
+a.right = ar;
+
+al.left = bl;
+al.right = br;
+
+ar.left = cl;
+ar.right = cr;
+cr.right = new BinaryTree(500);
+console.log(validBST(a));
+
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
