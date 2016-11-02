43b3761bf0551d2519b3553a56654fbc4aaef920
Dean Edwards <dmedwards4@gmail.com>
2016-11-02 09:24:47 -0700

    gcd hh

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
+  let result = 0;
 
+  for (let i = 0; i < Math.max(a, b); i++) {
+    if (a % i === 0 && b % i === 0) result = i;
+  }
+  
+  return result;
 }
+
+console.log(gcd(10, 9));
 
 module.exports = gcd;
