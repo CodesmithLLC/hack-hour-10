b7588c79a0baa76e3e17e927411984155d4aac2a
mlampedx <mlampesd@gmail.com>
2016-11-02 09:34:38 -0700

    gcd.js complete

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
-
+if (a % b === 0) {
+  return a;
 }
+	
+let divisor = Math.floor(Math.max(a, b) / 2);
+
+for (let i = divisor; i > 0; i--) {
+  if (a % i === 0 && b % i === 0) {
+  	return i;
+  }  
+}
+return 1;
+}
+
 
 module.exports = gcd;
