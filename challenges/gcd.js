060a04765970ca59b42a9a186f09d95dee04cdb2
Dan <dan.zot@gmail.com>
2016-11-02 09:35:28 -0700

    complete gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,19 @@
  */
 
 function gcd(a, b) {
-
+  const larger = a > b ? a : b;
+  const divisors = [];
+  for (let i = 0; i < larger; i++) {
+    if (a % i === 0 && b % i === 0) {
+      divisors.push(i);
+    }
+  }
+  return divisors.pop();
 }
+
+console.assert(gcd(10, 8) === 2)
+console.assert(gcd(10, 9) === 1)
+console.assert(gcd(54, 24) === 6)
+console.assert(gcd(210, 45) === 15)
 
 module.exports = gcd;
