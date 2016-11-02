1db289fa1924d05fdf84236b4b556040641a1208
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-02 09:42:31 -0700

    Euclid is OP

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,8 +7,24 @@
  *
  */
 
-function gcd(a, b) {
+// Euclid is OP
+const gcd = (a, b) => b ? gcd(b, a % b) : a;
 
-}
+// function gcd(a, b) {
+  
+//   // Need minimum value to begin looping through factors
+//   const min = Math.min(a, b);
+  
+//   // A GCD is found when both inputs are divisible by the same number
+//   for (let i = min; i > 0; i--) {
+      
+//     // The first shared factor will be the GCD since we're looping downward!
+//     if (a % i === 0 && b % i === 0) return i;
+//   }
+// }
+
+console.log(gcd(10, 8));
+console.log(gcd(10, 9));
+console.log(gcd(500, 400));
 
 module.exports = gcd;
