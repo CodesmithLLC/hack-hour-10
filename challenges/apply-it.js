d88716466a69e8afd4e4efa725221b072799440c
cesarhernar <cesarh@usc.edu>
2016-11-03 09:28:10 -0700

    finished apply it

---
+++ challenges/apply-it.js
@@ -0,0 +1,38 @@
+/**
+ * applies the invoked array to the function's parameter list
+ * Example:
+ *   var jae = function(name, age, location) {
+ *     return name + " is " + age + " and he lives in " + location;
+ *   };
+ *
+ *   var jaero = applyIt(jae, ["Jae", 19, "South Carolina"]);
+ *   jaero(); //Returns "Jae is 19 and he lives in South Carolina"
+ *
+ * Another Example:
+ *   var jasmine = function(name, age) {
+ *     if(!age){
+ *       return "We don't know how old " + name + " is!";
+ *     }
+ *
+ *     else{
+ *       return name + " is " + age + " years old!";
+ *     }
+ *   };
+ *
+ *   var jmoney = applyIt(jasmine, ["Jasmine"]);
+ *   jmoney(); //Returns "We don't know how old Jasmine is!"
+ *
+ *  DO NOT USE THE BUILT IN APPLY METHOD OR THE SPREAD OPERATOR
+ */
+
+function applyIt(func, args) {
+  if (args[0] !== undefined) {
+    var bind = func.bind(this, args[0]);
+    for (var i = 1; i < args.length; i++) {
+      bind = bind.bind(this, args[i]);
+    }
+    return bind;
+  }
+}
+
+module.exports = applyIt;
