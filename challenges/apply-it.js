3c53dcb1f74c08205fb159fe2a565276ba5feb9c
ryanbas21 <ryanbas21@gmail.com>
2016-11-03 09:29:03 -0700

    seeing what tests fail

--- challenges/apply-it.js
+++ challenges/apply-it.js
@@ -1,9 +1,7 @@
 /**
  * applies the invoked array to the function's parameter list
  * Example:
- *   var jae = function(name, age, location) {
- *     return name + " is " + age + " and he lives in " + location;
- *   };
+
  *
  *   var jaero = applyIt(jae, ["Jae", 19, "South Carolina"]);
  *   jaero(); //Returns "Jae is 19 and he lives in South Carolina"
@@ -25,8 +23,28 @@
  *  DO NOT USE THE BUILT IN APPLY METHOD OR THE SPREAD OPERATOR
  */
 
-function applyIt(func, args) {
-
+function applyIt(func, args) { let name,age,location
+  if (args[0] !== undefined)  name = args[0];
+  if (args[1] !== undefined)  age = args[1];
+  if (args[2] !== undefined)  location = args[2]
+  return (() => func.call(this,name,age,location)
+    //  func.call(this,name,age,location);
+  )();
 }
 
+  // var jae = function(name, age, location) {
+  //    return name + " is " + age + " and he lives in " + location;
+  //  };
+  //  applyIt(jae,["Jae", 19, "South Carolina"])
+
+   var jasmine = function(name, age) {
+        if(!age){
+          return "We don't know how old " + name + " is!";
+        }
+        else{
+          return name + " is " + age + " years old!";
+        }
+  };
+ var jmoney = applyIt(jasmine, ["Jasmine"]);
+  applyIt(jmoney,['Jasmine'])
 module.exports = applyIt;
