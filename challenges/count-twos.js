ab6a46dd06981973d2cd5482abb051da72df5292
Dan <dan.zot@gmail.com>
2016-11-04 09:16:38 -0700

    complete count-twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,7 +8,20 @@
 
 
 function countTwos(num) {
-
+  let twos = 0;
+  for (let i = 2; i <= num; i++) {
+    String(i).split('').forEach(elem => {
+      if (elem === '2') twos++;
+    });
+  }
+  return twos;
 }
+
+console.log(countTwos(1));  // -> 0
+console.log(countTwos(3));  // -> 1
+console.log(countTwos(13));  // -> 2
+console.log(countTwos(1000));  // -> 300
+console.log(countTwos(11420));  // -> 4483
+
 
 module.exports = countTwos;
