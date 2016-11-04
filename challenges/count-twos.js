50d5d9d709831504bc082203f320a4088131d8d9
Will Galebach <willbach@gmail.com>
2016-11-04 10:14:27 -0700

    finished numstring

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -1,4 +1,6 @@
-// Write a function "countTwos" that takes a number as an argument, n. The function will return the number of 2s encountered when counting from 1 to n (inclusive). 2222 counts as having 4 2s.
+// Write a function "countTwos" that takes a number as an argument, n. 
+// The function will return the number of 2s encountered when counting 
+// from 1 to n (inclusive). 2222 counts as having 4 2s.
 
 // countTwos(1);  -> 0
 // countTwos(3);  -> 1
@@ -8,7 +10,24 @@
 
 
 function countTwos(num) {
-
+  let numArr = num.toString().split('').reverse();
+  let count = 0;
+  for( let i = 0; i < numArr.length; i++ ) {
+    if( numArr[i] >= 3 ) {
+      count += Math.pow(10, i);
+    }
+    if( numArr[i] == 2 ) {
+      let numStr = '';
+      for( let j = i - 1; j >= 0; j-- ) {
+        numStr += numArr[j];
+      }
+      count += Number(numStr) + 1;
+    }
+    count += numArr[i] * Math.pow(10, (i - 1)) * i;
+  }
+  return count;
 }
+
+// console.log(countTwos(222));
 
 module.exports = countTwos;
