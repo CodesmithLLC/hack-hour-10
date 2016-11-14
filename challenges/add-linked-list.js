5d271ff90fac1c90ac66f8ce8c4c42b1f394509f
Dan <dan.zot@gmail.com>
2016-11-14 09:35:58 -0800

    complete add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,61 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  // convert both lists to numbers
+  let num1 = convertToNum(l1);
+  let num2 = convertToNum(l2);
+  // return list version of their sum
+  return convertToList(num1 + num2);
 
+  // helpler function converts a list to a number
+  function convertToNum(head) {
+    let cur = head;
+    let numStr = '';
+    while (cur) {
+      numStr = String(cur.value) + numStr;
+      cur = cur.next;
+    }
+    return Number(numStr);
+  }
+  
+  // helper function converts a number to a list
+  function convertToList(num) {
+    num = String(num).split('');
+    let head, cur;
+    while (num.length) {
+      const newNode = new Node(num.shift());
+      if (!head) {
+        head = newNode;
+        cur = head;
+      } else {
+        cur.next = newNode;
+        cur = cur.next;
+      }
+    }
+    return head;
+  }
 }
+
+// function printList(node) {
+//   let count = 0;
+//   while (node) {
+//     console.log(`node ${count++}: ${node.value}`);
+//     node = node.next;
+//   }
+// }
+//
+// let num1 = new Node(3);
+// num1.next = new Node(1);
+// num1.next.next = new Node(5);
+// console.log(convertToNum(num1));
+//
+// let num2 = new Node(5);
+// num2.next = new Node(9);
+// num2.next.next = new Node(3);
+// console.log(convertToNum(num2));
+
+// console.log(addLinkedList(num1, num2));
+
+// printList(convertToList(908));
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
