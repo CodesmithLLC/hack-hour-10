f13fb65639aa4812f1bffadbafd39512567f1f85
Dean Edwards <dmedwards4@gmail.com>
2016-11-14 09:42:06 -0800

    add-ll hh

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,7 +14,53 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let curr1 = l1;
+  let curr2 = l2;
 
+  let result = new Node(curr1.value + curr2.value);
+  let resCurr = result;
+
+  curr1 = curr1.next;
+  curr2 = curr2.next;
+  // console.log(curr1, curr2);
+  while (curr1 || curr2) {
+    let newVal;
+    if (curr1 && curr2) newVal = curr1.value + curr2.value;
+    else if (curr1 && !curr2) newVal = curr1.value;
+    else newVal = curr2.value;
+    
+    resCurr.next = new Node(newVal);
+    resCurr = resCurr.next;
+
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+    // console.log(curr1, curr2);
+  }
+
+  return result;
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+// var a = new Node(1);
+// var b = new Node(2);
+// var c = new Node(3);
+// var d = new Node(4);
+// var e = new Node(5);
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+// var f = new Node(1);
+// var g = new Node(2);
+// var h = new Node(3);
+// var i = new Node(4);
+// var j = new Node(5);
+
+// f.next = g;
+// g.next = h;
+// h.next = i;
+// i.next = j;
+
+// console.log(addLinkedList(a, f));
+module.exports = { Node: Node, addLinkedList: addLinkedList };
