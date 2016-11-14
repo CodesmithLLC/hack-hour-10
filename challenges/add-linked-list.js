6e9aac55b845b0eb700e5a2a26be8b115f1cdcf8
srei <steferu@gmail.com>
2016-11-14 09:44:35 -0800

    s'

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -6,15 +6,88 @@
  * Input: (3 -> 1 -> 5) + (5 -> 9 -> 2)
  * Output: 8 -> 0 -> 8
  *
+ * LINKED LIST CAN HAVE different lengths.
+ *
  */
+function LinkedList(){
+	this.head = null;
+	this.tail = null;
+	this.length = 0;
+}
 
 function Node(val) {
   this.value = val;
   this.next = null;
 }
 
-function addLinkedList(l1, l2) {
 
+LinkedList.prototype.push = function(val){
+	var newNode = new Node(val);
+
+	if(this.head === null){
+		this.head = newNode;
+	} else {
+		let currentnodebeinglookedat = this.head;
+		while(currentnodebeinglookedat.next){
+			currentnodebeinglookedat = currentnodebeinglookedat.next;
+		}
+		currentnodebeinglookedat.next = newNode;
+		this.tail = newNode;
+	}
+	this.length++;
 }
 
+function addLinkedList(l1, l2) {
+	var aggregateof1values = [];
+	var aggregateof2values = [];
+
+//going thru first linked list
+	var currentof1 = l1.head;
+	while(currentof1.next){
+		aggregateof1values.push(currentof1.value);
+		currentof1 = currentof1.next;
+	}
+	aggregateof1values.push(currentof1.value);
+
+	// go thru 2nd linked list
+	var currentof2 = l2.head;
+	while(currentof2.next){
+		aggregateof2values.push(currentof2.value);
+		currentof2 = currentof2.next;
+	}
+	aggregateof2values.push(currentof2.value);
+
+	//reverse the arrays;
+	var reversed1 = aggregateof1values.reverse().join("");
+	var reversed2 = aggregateof2values.reverse().join("");
+
+	//the new number
+  var newNum =  Number(reversed1) + Number(reversed2);
+ //convert back to string && separate the numbers
+ 	var stringNum = String(newNum)
+ var splitupnum = stringNum.split("");
+ //go thru array, change into numbers && create new linked list based off it
+ var newLinked = new LinkedList();
+ for(var i = 0; i < splitupnum.length; i++){
+ 	newLinked.push(Number(splitupnum[i]));
+ }
+return newLinked;
+}
+
+
+// var newList = new LinkedList();
+// newList.push(3);
+// newList.push(1);
+// newList.push(5);
+// // console.log(addLinkedList(newList));
+
+
+
+// var newList2 = new LinkedList();
+// newList2.push(5);
+// newList2.push(9);
+// newList2.push(2);
+// console.log(addLinkedList(newList, newList2));
 module.exports = {Node: Node, addLinkedList: addLinkedList};
+
+
