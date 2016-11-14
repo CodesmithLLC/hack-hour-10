aea66742a71891ab2ee0bf9bb8b05c8f790639b9
Ahmad K <lance@codemosey.net>
2016-11-14 09:17:30 -0800

    Does NOT implement count-twos.js

---
+++ challenges/count-twos.js
@@ -0,0 +1,12 @@
+// countTwos(1);  -> 0
+// countTwos(3);  -> 1
+// countTwos(13);  -> 2
+// countTwos(1000);  -> 300
+// countTwos(11420);  -> 4483
+
+
+function countTwos(num) {
+
+}
+
+module.exports = countTwos;
