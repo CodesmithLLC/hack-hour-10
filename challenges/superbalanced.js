a46de0adc49bd4b1cec4e485737a6bb8d954d288
travishuff <huff.travis@gmail.com>
2016-11-15 09:49:56 -0800

    superbalanced.js

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,53 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  if (!tree || !tree.root) {
+    return true;
+  }
+
+  let node = tree.root,
+    cache = {
+      min: Number.MAX_SAFE_INTEGER,
+      max: Number.MIN_SAFE_INTEGER
+    };
+
+  findDepth(cache, node, 0);
+  return cache.max - cache.min <= 1;
+}
+
+function findDepth(cache, node, depth) {
+  if (!node) {
+    if (depth < cache.min) {
+      cache.min = depth;
+    }
+    if (depth > cache.max) {
+      cache.max = depth;
+    }
+  } else {
+    findDepth(cache, node.left, depth + 1);
+    findDepth(cache, node.right, depth + 1);
+  }
 
 }
+
+// let tree = new BinaryTree(11);
+// let tree2 = new BinaryTree(7);
+// let tree3 = new BinaryTree(15);
+// tree.left = tree2;
+// tree.right = tree3;
+
+// let tree4 = new BinaryTree(5);
+// let tree5 = new BinaryTree(9);
+// tree2.left = tree4;
+// tree2.right = tree5;
+
+// let tree6 = new BinaryTree(13);
+// let tree7 = new BinaryTree(20);
+// tree3.left = tree6;
+// tree3.right = tree7;
+
+// console.log(tree);
+// console.log(superbalanced(tree));
+
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
