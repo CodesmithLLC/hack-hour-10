7c185de8c178f9fde5d98405663ad501dac95a63
Dean Edwards <dmedwards4@gmail.com>
2016-11-16 09:37:13 -0800

    repeat-number hh

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -15,12 +15,12 @@ function repeatNumbers(array) {
 
   for (let i = 0; i < array.length; i++) {
     if (!cache[array[i]]) {
-      cache[array[i]] = 1;
+      cache[array[i]] = cache[array[i]];
     }
     else return cache[array[i]];
   }
 }
 
-console.log(repeatNumbers([1,2,3,4,5,1,7,8,9,10]));
+// console.log(repeatNumbers([1,2,3,4,5,7,8,9,10,9]));
 
 module.exports = repeatNumbers;
