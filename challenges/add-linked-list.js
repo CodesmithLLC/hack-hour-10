0adb9474bb1f36224a9d59c34f3ac14815ed4f4c
Hoon Choi <vhchoi@gmail.com>
2016-11-16 09:08:58 -0800

    upload

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -14,46 +14,36 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
-  let arrayl1 = [];
-  let arrayl2 = [];
-  let ansArr = [];
-
+  let l1num = 0;
+  let l2num = 0;
+  let sum;
+  let sumArr;
   let currl1 = l1;
   let currl2 = l2;
 
-  let ansNode;
-
-  while (currl1) {
-    arrayl1.push(currl1.value);
-    currl1 = currl1.next;
+  while(currl1){
+      l1num += currl1.val;
+      currl1 = currl1.next;
   }
 
-  while (currl2) {
-    arrayl2.push(currl2.value);
-    currl2 = currl2.next;
+  while(currl2){
+      l2num += currl2.val;
+      currl2 = currl2.next;
   }
 
-  if (arrayl1.length > arrayl2.length) {
-    for (let i = 0; i < arrayl1.length; i++) {
-      if (arrayl2[i] === undefined) ansArr.push(arrayl1[i]);
-      ansArr.push(arrayl1[i] + arrayl2[i]);
-    }
-  } else if (arrayl2.length > arrayl1.length) {
-    for (let i = 0; i < arrayl2.length; i++) {
-      if (arrayl1[i] === undefined) ansArr.push(arrayl2[i]);
-      ansArr.push(arrayl12[i] + arrayl1[i]);
-    }
-  } else {
-    for (let i = 0; i < arrayl1.length; i++) {
-      ansArr.push(arrayl1[i] + arrayl2[i]);
-    }
+  sum = l1num + l2num;
+  sumArr = String(sum).split('');
+  let count = 0;
+  let newNode;
+
+  while(count < sumArr.length){
+    newNode = new Node(sumArr[count]);
+    newNode = newNode.next;
+    count++;
   }
 
-  for (let i = 0; i < ansArr.length; i++) {
-    if (ansNode === undefined) ansNode = new Node(ansArr[i]);
-    else ansNode.next = new Node(ansArr[i]);
-  }
-  return ansNode;
+  return newNode;
+  
 }
 
 module.exports = {
