9a4b7d5a5a70f09c1ef9aa717f1769f492d69578
Will Galebach <willbach@gmail.com>
2016-11-16 09:31:17 -0800

    finished in On time and O1 space

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,6 +14,7 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  if(!(tree instanceof BinaryTree)) return false;
   function height(tree) {
     if(!(tree instanceof BinaryTree)) return 0;
     let height = 0;
