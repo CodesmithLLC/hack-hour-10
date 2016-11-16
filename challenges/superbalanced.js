d238c9bd85a404b9256ec9f0694e33658c42ef67
Ahmad K <lance@codemosey.net>
2016-11-16 09:11:27 -0800

    begins to implement superbalanced.js

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -13,8 +13,36 @@ function BinaryTree(value) {
   this.right = null;
 }
 
-function superbalanced(tree) {
+BinaryTree.prototype.height = function(depthTraversed) {
+  if (!depthTraversed) depthTraversed = 0;
+  if (!this.left && !this.right) return depthTraversed;
+  if (!this.left)
+  return Math.max(this.left.height(depthTraversed + 1), this.right.height(depthTraversed + 1));
+}
 
+function superbalanced(tree) {
+  const leftDepth = 0;
+  const rightDepth = 0;
+  const currTree = tree;
+  while (currTree.left) {
+
+  }
+  // console.log('checking val:', tree.value);
+  // console.log(tree.left, tree.right);
+  // // if the tree has no children, it is inherently balanced
+  // if (!tree.left && !tree.right) return true;
+  // // check for obvious issues
+  // if (tree.left.value >= tree.value || tree.right.value <= tree.value) return false;
+  // if (!tree.left) return superbalanced(tree.right);
+  // if (!tree.right) return superbalanced(tree.left);
+  // return superbalanced(tree.left) && superbalanced(tree.right);
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+const myTree = new BinaryTree(2);
+myTree.left = new BinaryTree(1);
+myTree.right = new BinaryTree(4);
+myTree.right.left = new BinaryTree(3);
+myTree.right.right = new BinaryTree(5);
+myTree.right.right.right = new BinaryTree(6);
+console.log(myTree.height());
