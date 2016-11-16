4ec169bdc6909f15ac04930570bb4b256f0882e4
Martin-Ting <martinhting@gmail.com>
2016-11-16 09:25:20 -0800

    completed repeat number

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,23 @@
  */
 
 function repeatNumbers(array) {
-
+  let i = 0;
+  let runningSum = array.reduce((acc, curr) => {
+    i++;
+    return i === array.length - 1 ? acc : acc + i;
+  }, 0);
+  // console.log('========');
+  // console.log(array);
+  // console.log(array.length);
+  // console.log(runningSum);
+  // console.log('========');
+  let totalSum = array.reduce((acc, curr) => {
+    return acc + curr;
+  }, 0)
+  // console.log(totalSum);
+  return totalSum - runningSum + 1;
 }
+
+// console.log(repeatNumbers([2,4,6,8,1,3,5,2,7,9]));
 
 module.exports = repeatNumbers;
