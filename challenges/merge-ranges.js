4c7185fd501507bd1056ca9634b88d2a1f92de28
mlampedx <mlampesd@gmail.com>
2016-11-17 09:46:22 -0800

    merge-ranges.js complete

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,27 @@
 
 
 function mergeRanges(array) {
-
+if (array.constructor !== Array) {
+  throw new Error('Input must be an array.');
 }
+
+let sortedArr = array.sort((arr1, arr2) => {
+  return arr1[0] - arr2[0];
+})
+let temp = [];
+
+for (let i = 0; i < sortedArr.length - 1; i++) {
+  if (sortedArr[i][1] >= sortedArr[i + 1][0]) {
+    temp.push(sortedArr[i][0]);
+    temp.push(sortedArr[i + 1][1]);
+    sortedArr.splice(i, 2);
+    sortedArr.splice(i, 0, temp);
+    temp = [];
+    i--;
+  }
+}
+return sortedArr;
+}
+
 
 module.exports = mergeRanges;
