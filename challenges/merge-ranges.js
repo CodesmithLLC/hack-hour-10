b8532240edb5e8c08dcc8f96becd883c8e179833
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-17 09:34:55 -0800

    nlogn solution using sort, shift and reduce

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,21 @@
 
 
 function mergeRanges(array) {
+  // Sort by start times
+  const sorted = array.sort((a, b) => a[0] - b[0]);
 
+  // Remove start
+  const start = sorted.shift();
+
+  // If current start time is greater than last end time, create new time slot.
+  // Otherwise, combine times by overwriting last end time with new end time.
+  return sorted.reduce((acc, curr) => {
+    curr[0] > acc[acc.length - 1][1] ? acc.push(curr) : acc[acc.length - 1][1] = curr[1];
+    return acc;
+  }, [start]);
 }
+
+// var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]];
+// console.log(mergeRanges(times)); //-> [[0, 1], [3, 8], [9, 12]]
 
 module.exports = mergeRanges;
