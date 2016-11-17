9364c735b51a9964504ad5d6881c2e8d523f6b88
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-11-17 09:15:52 -0800

    Thai's merge-ranges

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,14 @@
 
 
 function mergeRanges(array) {
-
+    return array.sort((a, b) => { return a[0] > b[0] }).reduce((a, c, i, s) => {
+        if (a[a.length - 1][1] >= c[0]) a[a.length - 1] = [a[a.length - 1][0], a[a.length - 1][1] >= c[1] ? a[a.length - 1][1] : c[1]]
+        else a.push(c)
+        return a
+    }, [array[0]])
 }
 
 module.exports = mergeRanges;
+
+var times = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+console.log(mergeRanges(times))
