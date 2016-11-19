085d0ce9008a2c80857c56599c2878e60545d914
Dan <dan.zot@gmail.com>
2016-11-19 09:47:16 -0800

    complete max-subarray

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,18 @@
  */
 
 function maxSubarray(arr) {
-
+  const sums = [];
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum += arr[j];
+      sums.push(sum);
+    }
+  }
+  return Math.max(...sums);
 }
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+// console.log(maxSubarray([15,20,-5,10]));
 
 module.exports = maxSubarray;
