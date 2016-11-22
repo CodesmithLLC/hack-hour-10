222f9422f7a873cb61eef66d6a292857ef91a61a
Will Galebach <willbach@gmail.com>
2016-11-22 09:49:14 -0800

    finished anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,33 @@
   */
 
 function anagrams(string) {
-
+  var result = [];
+  function find(n, arr) {
+    if(n === 1) {
+      result.push(arr.join(''));
+      return;
+    }
+    else {
+      for(let i = 0; i < n-1; i++) {
+        find(n - 1, arr);
+        if( n % 2 === 0) {
+          var temp = arr[i];
+          arr[i] = arr[n-1];
+          arr[n-1] = temp;
+        }
+        else {
+          var temp = arr[0];
+          arr[0] = arr[n-1];
+          arr[n-1] = temp;
+        }
+      }
+      find(n - 1, arr);
+    }
+  }
+  find(string.length, string.split(''));
+  return result;
 }
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
