2402502de9e84b19433fbb0b725693ec557e6101
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-22 09:47:14 -0800

    Heap's algorithm

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,52 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+// Heap's algorithm
+function anagrams(string) { 
+  let arr = Array.from(string);
+  const permutations = [];
 
-}
+  function swap(a, b) {
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  function generate(n) {
+    if (n === 1) permutations.push(arr.join());
+    else {
+      for (let i = 0; i !== n; ++i) {
+        generate(n - 1);
+
+        // Odd length swaps first element. Even length swaps ith element.
+        swap(n % 2 ? 0 : i, n - 1);
+      }
+    }
+  }
+
+  generate(arr.length);
+  return permutations;
+}   
+
+// Brute force solution
+// function anagrams(string) {
+//   function permutate(str, memo = []) {
+//     const arr = Array.from(str);
+//     let curr;
+
+//     for (let i = 0; i < arr.length; i++) {
+//       curr = arr.splice(i, 1);
+//       if (!arr.length) result.push(memo.concat(curr));
+//       permutate(arr.slice(), memo.concat(curr));
+//       arr.splice(i, 0, curr[0]);
+//     }
+//   }
+
+//   const result = [];
+//   permutate(string);
+//   return result;
+// }
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
