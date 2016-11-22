93fe5ef8427fb7a6b9f061e9684ffe0b7eca119e
Edward Guo <edward.h.guo@gmail.com>
2016-11-22 09:20:08 -0800

    anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,28 @@
   */
 
 function anagrams(string) {
-
+  const perms = [];
+  const useds = [];
+  function perm(str) {
+    let i, ch, chars = str.split('');
+    for (i = 0; i < chars.length; i += 1) {
+      ch = chars.splice(i, 1);
+      useds.push(ch);
+      if (!chars.length) perms[perms.length] = useds.join('');
+      perm(chars.join(''));
+      chars.splice(i, 0, ch);
+      useds.pop();
+    }
+  }
+  function unique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+  perm(string);
+  const uniqs = perms.filter(unique);
+  return uniqs;
 }
 
+
 module.exports = anagrams;
+
+console.log(anagrams('aac'));
