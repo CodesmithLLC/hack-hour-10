bdb68c0cdb7fe80f2396bebb7bd3b052bd20f2ba
ryanbas21 <ryanbas21@gmail.com>
2016-11-22 09:49:47 -0800

    saving

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,18 @@
   */
 
 function anagrams(string) {
+  const anagram = [];
+  string.split(' ').forEach( (str) => {
+    const recurse =  (ana, str) => {
+      if (str === '') anagram.push(ana);
+      str.split('').forEach((item, i) => recurse(ana + str[i], str.slice(0, i) + str.slice(i + 1)));
+    };
+    recurse('', str);
+  });
+  return anagram;
+  }
 
-}
 
+
+anagrams('abc');
 module.exports = anagrams;
