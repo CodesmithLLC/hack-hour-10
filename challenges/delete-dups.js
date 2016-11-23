21d9080a291a18ca4c661c98b3183589728f3dfd
Michael Sotkin <michael.sotkin@gmail.com>
2016-11-23 11:46:09 -0800

    finished hack hour

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Write a function deleteDups that removes duplicates from an unsorted linked list.
  *
@@ -13,7 +14,38 @@
 
 
 function deleteDups(head) {
+  let store = [];
+  let node = head;
+  let prev;
+  let value;
 
+  while (node) {
+    if (store.indexOf(node.value) !== -1) {
+      prev.next = node.next;
+    }
+    store.push(node.value);
+    prev = node;
+    node = node.next;
+  }
+
+  return head;
 }
+
+let a = {value: 1};
+// let b = {value: 2};
+// let c = {value: 3};
+// let d = {value: 3};
+// let e = {value: 4};
+let f = {value: 1};
+
+a.next = f;
+// // b.next = c;
+// c.next = d;
+// // d.next = null;
+// d.next = e;
+// e.next = f;
+f.next = null;
+
+console.log(deleteDups(a));
 
 module.exports = deleteDups;
