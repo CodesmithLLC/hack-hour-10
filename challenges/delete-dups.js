ab97d192238a602dc46fa1a18ed8319f08e75bb3
George Norberg <georgenorberg@Georges-MacBook-Pro.local>
2016-11-23 11:28:20 -0800

    deleted dupes with temporary buffer

--- challenges/delete-dups.js
+++ challenges/delete-dups.js
@@ -10,10 +10,41 @@
  * How would you solve this problem if a temporary buffer is not allowed?
  */
 
-
+function Node(value) {
+  this.value = value;
+  this.next = null;
+}
 
 function deleteDups(head) {
 
+  // Store values to check dupes while traversing  
+  const values = {};
+  
+  // Helper function that deletes nodes by redirecting pointers
+  const removeNextNode = node => node.next = node.next.next ? node.next.next : null;
+
+  // Checks if next node's value is a dupe and either deletes or stores value
+  function traverse(LL) {
+    if (!LL.next) return;
+    values[LL.next.value] ? removeNextNode(LL) : values[LL.next.value] = 1;
+    traverse(LL.next);
+  }
+
+  traverse(head);
+  return head;
 }
+
+const a = new Node(1);
+const b = new Node(2);
+const c = new Node(2);
+const d = new Node(3);
+const e = new Node(4);
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(deleteDups(a));
 
 module.exports = deleteDups;
