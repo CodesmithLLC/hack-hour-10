d0042ad1154d994725cbe2468308d3765821e598
cesarhernar <cesarh@usc.edu>
2016-11-22 22:11:02 -0800

    finished anagram

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -13,7 +13,24 @@
   */
 
 function anagrams(string) {
-
+  var store = [];
+  var total = 1;
+  for (var i = string.length; i > 0; i--) { 
+    total *= i;
+  }
+  var holder = string.split('');
+  function permute(array, storing) {
+    if (storing.length === total) return null;
+    else {
+      storing.push(array.join(''));
+      permute(array.slice(1).concat(array[0]), storing)
+      var lastIndex = array.length - 1
+      permute(array[lastIndex].concat(array.slice(0, lastIndex)), storing)
+    }
+  }
+  permute(holder, store); 
+  
+  return store;
 }
 
 module.exports = anagrams;
