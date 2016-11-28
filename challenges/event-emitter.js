1983d03ef64f99a3926bbf51b478e204e1268211
Dan <dan.zot@gmail.com>
2016-11-28 09:34:34 -0800

    complete event-emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -21,16 +21,41 @@
  * - It is not necessary to write a way to remove listeners.
  */
 
-function EventEmitter() {
+ function EventEmitter() {
+   this.functions = [];
+ }
 
-}
+ EventEmitter.prototype.on = function(funcName, func) {
+   const event = {
+     function: funcName,
+     callback: func
+   };
+   this.functions.push(event);
+ };
 
-EventEmitter.prototype.on = function(funcName, func) {
+ EventEmitter.prototype.trigger = function(funcName, ...args) {
+   this.functions
+     .filter(e => e.function === funcName)
+     .forEach(e => e.callback(...args));
+ };
 
-};
+ // var instance = new EventEmitter();
+ // var counter = 0;
+ // instance.on('increment', function() {
+ //   counter++;
+ // }); // counter should be 0
+ // instance.trigger('increment'); // counter should be 1
+ // console.log(counter);
+ // instance.trigger('increment'); // counter should be 2
+ // console.log(counter);
+ //
+ // instance.on('add', (x) => {counter += x});
+ // instance.trigger('add', 10);
+ // console.log(counter);
+ //
+ // instance.on('increment', () => {counter -= 2});
+ // instance.trigger('increment');
+ // console.log(counter);
 
-EventEmitter.prototype.trigger = function(funcName, ...args) {
-
-};
 
 module.exports = EventEmitter;
