697176aa80b9041cbc024e01e7f0941c2a06207a
Edward Guo <edward.h.guo@gmail.com>
2016-11-28 09:47:38 -0800

    event emitter

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -25,12 +25,24 @@ function EventEmitter() {
   this.e = {};
 }
 
-EventEmitter.prototype.on = function(funcName, func) {
-  this.e[funcName] = func;
+EventEmitter.prototype.on = function (funcName, func) {
+  if (!this.e[funcName]) this.e[funcName] = [func];
+  else this.e[funcName].push(func);
 };
 
-EventEmitter.prototype.trigger = function(funcName, ...args) {
-  this.e[funcName].apply(this, ...args);
+EventEmitter.prototype.trigger = function (funcName, ...args) {
+  this.e[funcName].forEach(func => func.apply(this, ...args));
 };
 
 module.exports = EventEmitter;
+
+var instance = new EventEmitter();
+var counter = 0;
+instance.on('increment', function() {
+  counter++;
+});
+console.log('Counter should be 0 ', counter);
+instance.trigger('increment');
+console.log('Counter should be 1 ', counter);
+instance.trigger('increment');
+console.log('Counter should be 2 ', counter);
