343e168b709812ba057d86b4b5376ee0caa0fa9a
cesarhernar <cesarh@usc.edu>
2016-11-28 10:45:43 -0800

    finished event emitters

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,37 @@
  */
 
 function EventEmitter() {
+  this.events = {};
+  // this.on = (event, callback) => { 
+  //   if (!this.events[event]) this.events[event] = [callback];
+  //   else this.events[event].push(callback);
+  // }
+  // this.trigger = (event, ...args) => { 
+  //   var callbacks = this.events[event];
+  //   callbacks.forEach((fn) => {
+  //     fn(...args);
+  //   })
+  // }
 
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  if (!this.events[funcName]) this.events[funcName] = [func];
+  else this.events[funcName].push(func);
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+var callbacks = this.events[funcName];
+callbacks.forEach((fn) => {
+  fn(...args);
+})
 };
-
+// var instance = new EventEmitter();
+// var counter = 0;
+// instance.on('increment', function() {
+//    counter++;
+//  }); // counter should be 0
+//  instance.trigger('increment'); // counter should be 1
+//  instance.trigger('increment')
+//  console.log(counter)
 module.exports = EventEmitter;
