3cf54ceb34297e4519c84091f2aa5183dfef57d3
Ahmad K <lance@codemosey.net>
2016-11-29 09:26:41 -0800

    implements findInOrderedSet()

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,25 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  let currArr = arr;
+  while (currArr.length) {
+    let index = Math.floor(currArr.length / 2);
+    // console.log(`Pulled ${currArr[index]} out for examination.`);
+    if (currArr[index] === target) return true;
+    if (target < currArr[index]) {
+      // console.log('Target is less than current Index, grabbing front half.');
+      currArr = currArr.slice(0, index);
+    } else currArr = currArr.slice(index + 1, currArr.length);
+  }
+  // console.log('Got to the end, never saw target');
+  return false;
 }
 
 
 module.exports = findInOrderedSet;
+
+// Testing follows
+
+// var nums = [1, 4, 6, 7, 9, 10, 11, 12, 13, 14, 17, 45, 99, 1024];
+// console.log('Return:' , findInOrderedSet(nums, 14));
+// console.log('Return:' , findInOrderedSet(nums, 99));
