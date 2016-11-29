96ff29148db7477590558f60022dee088d6b22fb
Michael Sotkin <michael.sotkin@gmail.com>
2016-11-29 09:49:19 -0800

    finished hack hour

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,28 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  let L = 0;
+  let R = arr.length - 1;
+  let m = Math.floor((L + R) / 2);
+  
+  if (target < arr[0] || target > arr[arr.length - 1] || arr.length === 0) return false;
 
+  function recurser(L, R, m) {
+    if (L > R) return false;
+    if (arr[m] < target) {
+      L = m + 1;
+      m = Math.floor((L + R) / 2);
+      return recurser(L, R, m);
+    }
+    if (arr[m] > target) {
+      R = m - 1;
+      m = Math.floor((L + R) / 2);
+      return recurser(L, R, m);
+    }
+    if (arr[m] === target) return true;
+  }
+  
+  return recurser(L,R,m);
 }
-
 
 module.exports = findInOrderedSet;
