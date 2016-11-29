6db96536ef65644efd1da157092e71167d134c1c
David Wilson <w.dave.w@gmail.com>
2016-11-29 09:39:49 -0800

    find-in-ordered-set no slicing

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,18 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
 function findInOrderedSet(arr, target) {
-
+  function bisectionSearch(start, end) {
+    if (start === end) return (arr[start] === target)
+    if (start > end) return false;
+    const midpoint = Math.floor((end + start) / 2);
+    if (arr[midpoint] === target) return true;
+    else if (arr[midpoint] < target) return bisectionSearch(midpoint + 1, end);
+    else return bisectionSearch(start, midpoint - 1);
+  }
+  return  bisectionSearch(0, arr.length - 1);
 }
 
+var nums = [1, 4, 6, 7, 9, 17, 45]
 
 module.exports = findInOrderedSet;
