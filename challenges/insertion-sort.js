f0e8b8e5680cc58b7ea2a72d254593c9cd5dc3ef
Nick Nelson <ntylernelson@gmail.com>
2016-11-30 09:21:52 -0800

    11-30-16

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+  if(array.length < 2) return array;
+  for(let i = array.length; i > 0; i--) {
+    for(let j = i - 1; j > -1; j--){
+      if(array[j] > array[i]){
+        let swap = array[i];
+        array[i] = array[j];
+        array[j] = swap;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
