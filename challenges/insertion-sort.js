d69f17434a0b16838a4d44a4bc532ee2742ef58f
Nick Nelson <ntylernelson@gmail.com>
2016-11-30 10:02:22 -0800

    11-30-16 Hack

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,12 +6,11 @@
 
 function insertionSort(array) {
   if(array.length < 2) return array;
-  for(let i = array.length; i > 0; i--) {
+  for(let i = 0; i < array.length; i++) {
     for(let j = i - 1; j > -1; j--){
-      if(array[j] > array[i]){
-        let swap = array[i];
-        array[i] = array[j];
-        array[j] = swap;
+      if(array[i] < array[j] && array[i] > array[j - 1] || j - 1 == -1){
+        array.splice(j, 0, array[i]);
+        array.splice(i + 1, 1);
       }
     }
   }
