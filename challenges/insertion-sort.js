18758ae4e012bb42b9cf6efd04e9cbaa13f83c20
ryanbas21 <ryanbas21@gmail.com>
2016-11-30 09:13:06 -0800

    saving

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,17 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (array[i] < array[j]) {
+        const spliced = array.splice(i, 1);
+        array.splice(j, 0, spliced[0]);
+      }
+    }
+  }
+  console.log(array)
+  return array;
 }
-
+const arr = [10,5,3,8,2,6,4,7,9,1]
+insertionSort(arr)
 module.exports = insertionSort;
