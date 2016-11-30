88e6fedd80b453ecf63ba1d6232991a08106a32a
Edward Guo <edward.h.guo@gmail.com>
2016-11-30 10:12:09 -0800

    insertion

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,24 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
+function swap(array, one, two) {
+  const temp = array[one];
+  array[one] = array[two];
+  array[two] = temp;
+}
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let one = i, two = i - 1;
+    while (array[one] < array[two]) {
+      swap(array, one, two);
+      one-- , two--;
+    }
+  }
 }
 
 module.exports = insertionSort;
+
+let array = [6, 5, 3, 1, 8, 7, 2, 4];
+insertionSort(array);
+console.log(array);
