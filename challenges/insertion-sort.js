2e1f16c80cd9c4b349c3aa878da08f485c0ac225
Will Galebach <willbach@gmail.com>
2016-11-30 09:28:08 -0800

    finished insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,16 @@
 
 
 function insertionSort(array) {
-
+  for(let i = 1; i < array.length; i++) {
+    for(let j = i; j > 0, array[j-1] > array[j]; j--) {
+      let temp = array[j];
+      array[j] = array[j-1];
+      array[j-1] = temp;
+    }
+  }
+  return array;
 }
+
+console.log(insertionSort([4, 3, 2, 1]))
 
 module.exports = insertionSort;
