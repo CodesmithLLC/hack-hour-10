af617f7afa791e54eec74c82c1142c28520fa291
Dean Edwards <dmedwards4@gmail.com>
2016-11-30 09:29:41 -0800

    insertion sort hh:

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,19 @@
 
 
 
-function insertionSort(array) {
-
+function insertionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i];
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
 }
 
+// console.log(insertionSort([5, 4, 3, 2, 1]));
+
 module.exports = insertionSort;
+
