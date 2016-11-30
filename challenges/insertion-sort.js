aa1c36337ac6d07712bc6131544818f1433d2e66
Edward Guo <edward.h.guo@gmail.com>
2016-11-30 10:14:53 -0800

    insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -1,10 +1,11 @@
-function swap(array, one, two) {
-  const temp = array[one];
-  array[one] = array[two];
-  array[two] = temp;
-}
 
 function insertionSort(array) {
+  function swap(array, one, two) {
+    const temp = array[one];
+    array[one] = array[two];
+    array[two] = temp;
+  }
+  
   for (let i = 1; i < array.length; i++) {
     let one = i, two = i - 1;
     while (array[one] < array[two]) {
