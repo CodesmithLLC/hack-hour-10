5ba30a354def8e3db30c603dceae1e5ce3472a75
Thai-Duong Nguyen <soleiluwedu@Not-an-NSA-Surveillance-Node.local>
2016-12-01 09:15:52 -0800

    Thai's unique-number

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,10 @@
  *
  */
 function uniqueNumber(array) {
-
+    const sorted = array.sort()
+    for (let i = 0; i < sorted.length; i += 2) {
+        if (sorted[i] !== sorted[i+1]) return sorted[i]
+    }
 }
 
 module.exports = uniqueNumber;
