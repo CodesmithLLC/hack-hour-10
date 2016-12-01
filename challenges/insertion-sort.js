1cfd3db62a14afec0b3354e3e71c0d491025638e
Cheng Ly <chengsieuly@gmail.com>
2016-12-01 13:05:44 -0800

    feat: Finish HH W7D3 insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,28 @@
 
 
 function insertionSort(array) {
+  // Pointer at current element
+  // Next pointer and next element relative to current
+  // Compare current and next pointers
+  // If next is greater than current, then swap numbers
+  // After swap, check backwards with a previous pointer
+  // // ends when current is greater than previous
 
+  for (let i = 1; i < array.length; i++) {
+    function swap(current, previous, idx) {
+      array[idx] = previous;
+      array[idx - 1] = current;
+    }
+
+    for (let j = i; j >= 1; j--) {
+      const current = array[j];
+      const previous = array[j - 1];
+      if (current >= previous) break;
+      swap(current, previous, j);
+    }
+  }
+
+  return array;
 }
 
 module.exports = insertionSort;
