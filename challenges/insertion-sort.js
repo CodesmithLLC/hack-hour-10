d164383c9c8da95a82c6e26d080e30f270453735
Ahmad K <lance@codemosey.net>
2016-12-01 22:15:41 -0800

    implements insertion-sort and unique-number. fixes event-emitter.

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -2,10 +2,19 @@
 
 // https://en.wikipedia.org/wiki/Insertion_sort
 
-
-
 function insertionSort(array) {
-
+  const unsortedArray = array.slice();
+  const result = unsortedArray.reduce((sortedArray, numToPlace) => {
+    let index = 0;
+    while (index < sortedArray.length) {
+      if (numToPlace <= sortedArray[index]) break;
+      index++;
+    }
+    // console.log(`inserting ${numToPlace} at index ${index}`);
+    return sortedArray.slice(0, index).concat(numToPlace).concat(sortedArray.slice(index, sortedArray.length));
+  }, []);
+  return result;
 }
 
 module.exports = insertionSort;
+// console.log(insertionSort([9, 1, 5, 2, 7, 9, 3, 1]));
--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,23 @@
  *
  */
 function uniqueNumber(array) {
-
+  const count = array.reduce((countObj, id) => {
+    countObj[id] = ++countObj[id] || 1;
+    return countObj;
+  }, {});
+  // console.log('CountObj: ', count);
+  return Object.keys(count).reduce((uniqueId, id) => {
+    // console.log('uniqueId: ', uniqueId);
+    if (count[id] === 1) return id;
+    return uniqueId;
+  }, false);
 }
 
 module.exports = uniqueNumber;
+
+// testing
+// console.log(uniqueNumber([1, 2, 3, 1, 3]));
+// console.log(uniqueNumber([1, 2, 3, 1, 2]));
+// console.log(uniqueNumber([1, 2, 2, 1, 3]));
+// console.log(uniqueNumber([1, 2, 2, 3, 3]));
+// console.log(uniqueNumber([1, 2, 3, 1, 3, 5, 5, 1, 4, 4, 7, 2]));
