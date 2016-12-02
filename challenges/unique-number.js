96e30e52ad25a7b0fe830f002793bd7f2cd04f96
George Norberg <george.norberg@gmail.com>
2016-12-01 19:35:44 -0800

    bitwise operators are black magic

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -1,6 +1,6 @@
 /**
  * Given an array of IDs, which contains many duplicate (there are two) integers and one unique integer,
- * find the unqiue integer.
+ * find the unique integer.
  *
  * uniqueNumber([1,2,1,3,3]); -> 2
  *
@@ -9,8 +9,20 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
 
-}
+// Binary XOR operator yields unique value (in decimal) after all bitwise differences are taken into account
+const uniqueNumber = array => array.reduce((acc, cur) => acc ^ cur, 0);
+
+// const arr1 = [1, 2, 3, 2, 1];
+// const arr2 = [1, 2, 1, 3, 3];
+// const arr3 = [0, 15, 7, 0, 7, 6, 6];
+// const arr4 = [3];
+// const arr5 = [1, 3, 3];
+
+// console.log(uniqueNumber(arr1)); // 3
+// console.log(uniqueNumber(arr2)); // 2
+// console.log(uniqueNumber(arr3)); // 15
+// console.log(uniqueNumber(arr4)); // 3
+// console.log(uniqueNumber(arr5)); // 1
 
 module.exports = uniqueNumber;
