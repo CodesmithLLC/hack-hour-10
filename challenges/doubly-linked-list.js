291b23ab3b82344a94f9d7617bf74b3f19f5e67d
Edward Guo <edward.h.guo@gmail.com>
2016-12-03 09:15:03 -0800

    doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,34 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  const newNode = new Node(val);
+  if (!head) this.head = this.tail = newNode;
+  else {
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let currNode = this.head;
+  let prevNode;
+  if (currNode.val === val) {
+    this.head = currNode.next;
+    return currNode;
+  }
+  while (currNode) {
+    prevNode = currNode;
+    currNode = currNode.next;
+    if (currNode.val === val) {
+      prevNode.next = currNode.next;
+      currNode.next = prevNode;
+      return currNode;
+    }
+  }
 };
 
 module.exports = LinkedList;
