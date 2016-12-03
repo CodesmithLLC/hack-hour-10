fb1f3f296357761eb13cfa7b339ef24d85cdef9f
ryanbas21 <ryanbas21@gmail.com>
2016-12-03 09:14:03 -0800

    save

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,23 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  const newNode = new Node(val);
+  if (this.head) this.tail = newNode;
+  else this.head = this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let current = this.head;
+  while (current.next && current.value !== val) {
+    current = current.next;
+  }
+  if (current.value === val) {
+    current.next = current.next.next;
+  }
 };
 
 module.exports = LinkedList;
