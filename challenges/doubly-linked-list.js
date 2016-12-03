3957211cb9cd349c7012dadbcb4bac03e76efe4e
Michael Sotkin <michael.sotkin@gmail.com>
2016-12-03 09:37:45 -0800

    finished doubly linked list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,32 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
+  let node = this.head;
+  if (!this.head) this.head = this.tail = new Node(val);
+  else {
+    while (node.next) {
+      node = node.next;
+    }
+    node.next = this.tail = new Node(val);
+    node.next.prev = node;
+  }
   
+  return true;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let node = this.head;
+  while (node) {
+    if (node.val === val) {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+      return node;
+    }
+    node = node.next;
+  }
 };
 
 module.exports = LinkedList;
