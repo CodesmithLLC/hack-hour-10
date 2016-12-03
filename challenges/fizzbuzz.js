bd7a057e75a104f9559271ff7b670cbe1254f134
Edward Guo <edward.h.guo@gmail.com>
2016-12-02 19:00:58 -0800

    fizzbuzz one line ternary

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,12 +1,5 @@
 function fizzbuzz(n) {
-  const fb = [];
-  for (let i = 1; i <= n; i++) {
-    const f = i % 3 === 0, b = i % 5 === 0;
-    f ?
-      b ? fb.push('fizzbuzz') : fb.push('fizz') :
-      b ? fb.push('buzz') : fb.push(i);
-  }
-  return fb;
+  return Array.from(Array(n)).map((e, i, a) => a[i] = (i + 1) % 3 ? (i + 1) % 5 ? (i + 1) : 'buzz' : (i + 1) % 5 ? 'fizz' : 'fizzbuzz');
 }
 
 module.exports = fizzbuzz;
