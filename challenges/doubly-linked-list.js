cc45d45783f839f1cb7ecfa4996044b2ac3c9881
Andres Olivero <andres.eolivero@gmail.com>
2016-12-03 09:38:18 -0800

    doubly linked list hack hour

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,28 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  this.tail.next = new Node(val);
+  this.tail.next.prev = this.tail;
+  this.tail = this.tail.next;
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let current = this.head;
+  while (current) {
+    if (current.value === val) {
+      let prevTemp = current.prev;
+      let nextTemp = current.next;
+      if (nextTemp) nextTemp.prev = prevTemp;
+      if (prevTemp) prevTemp.next = nextTemp;
+      if (current === this.head && nextTemp) this.Head = nextTemp;
+      if (current === this.tail && prevTemp) this.Tail = prevTemp;
+      return current;
+    }
+    current = current.next;
+  }
 };
 
 module.exports = LinkedList;
