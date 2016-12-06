ac815286ee39fb89961d27ea05c729aef9357771
Edward Guo <edward.h.guo@gmail.com>
2016-12-06 09:23:02 -0800

    bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -1,11 +1,15 @@
-// Write a function that sorts an array using the bubble sort algorithm
-
-// https://en.wikipedia.org/wiki/Bubble_sort
-
-
-
 function bubbleSort(array) {
+  function swap(one, two) {
+    const temp = array[one];
+    array[one] = array[two];
+    array[two] = temp;
+  }
 
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[j - 1] > array[j]) swap(j - 1, j);
+    }
+  }
 }
 
 module.exports = bubbleSort;
