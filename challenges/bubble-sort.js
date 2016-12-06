c1a75a36531b1c16c0428394b95ef87b64cd7592
srei <steferu@gmail.com>
2016-12-06 09:31:26 -0800

    bubble

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -1,11 +1,30 @@
 // Write a function that sorts an array using the bubble sort algorithm
+// INFO:
+	// 	the sort that steps through the list to be sorted, compares adjacent items, and swaps them if they are in the wrong order. this pass thru list is repeated until no swaps are needed.
+	// it is very slow, even slower than insertion sort === it is O(n)squared.
+	// it's one advantage is that when the list IS already orted, the complexity goes down to O(n)
+//
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
 
 function bubbleSort(array) {
+	let length = array.length;
+	let wasSwapped = false;
 
+	do {
+		wasSwapped = false;
+			for ( let i = 0; i < length-1; i++) {
+				if (array[i] > array[i+1]) {
+					let temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+					wasSwapped = true;
+				}
+			}
+	} while (wasSwapped);
+
+	return array;
 }
-
 module.exports = bubbleSort;
