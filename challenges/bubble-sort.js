80161861f5275493a709702f86214201e15a9093
cesarhernar <cesarh@usc.edu>
2016-12-06 09:20:58 -0800

    finish bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+  var length = array.length;
+  function sortHelpert(arr, cont) {
+    var keepgoing = false;
+    if (!cont) return arr;
+    else { 
+      for (var i = 0; i < length - 1; i++) {
+        var holder = arr[i];
+        if (arr[i] > arr[i + 1]) {
+          arr[i] = arr[i + 1];
+          arr[i + 1] = holder;
+          keepgoing = true;
+        }
+      }
+      return sortHelpert(arr, keepgoing);
+    }
+  }
+  return sortHelpert(array, true);
 }
-
 module.exports = bubbleSort;
