a3bd05044ea6843353aad4945c78d19816292779
Cheng Ly <chengsieuly@gmail.com>
2016-12-06 09:32:52 -0800

    feat: Finish HH W8D2 bubble-sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,23 @@
 
 
 function bubbleSort(array) {
+  if (!Array.isArray(array)) return null;
+  if (array.length < 2) return array;
 
+  for (let i = 0; i < array.length - 1; i++) {
+    let isSorted = true;
+
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        isSorted = false;
+      }
+    }
+
+    if (isSorted) return array;
+  }
+
+  return array;
 }
 
 module.exports = bubbleSort;
