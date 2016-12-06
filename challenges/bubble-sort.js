7907477b8e85763c2492872a737966f037248783
Nick Nelson <ntylernelson@gmail.com>
2016-12-06 09:25:27 -0800

    12-6-16

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,13 @@
 
 
 function bubbleSort(array) {
-
+  for(let i = 0; i < array.length - 1; i++) {
+    if(array[i] > array[i + 1]) {
+      [array[i], array[i + 1]] = [array[i + 1],  array[i]];
+      i = 0;
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;
