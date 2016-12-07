8d1ceb69e709c7d2915a74a84ec7dd5355068222
Edward Park <ed.sh.park@gmail.com>
2016-12-07 09:44:57 -0800

    completed phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -22,26 +22,17 @@
 const findName = (jazbook, name) => jazbook.reduce((a, c) => name === c[0] ? c[1] : a, false);
 
 
-let jazbook = [
-  ['alex', '301-844-3421'],
-  ['jae', '301-844-1211'],
-  ['david', '301-844-0978'],
-  ['travis', '301-844-8505'],
-  ['jasmine', '1800-974-4539'],
-];
-
-console.log(findName(jazbook, 'eddie'));
-
 // return an object literal representing the jazbook
 const makePhoneBookObject = (jazbook) => {
   let obj = {};
 
   jazbook.forEach(entry => {
-    entry[0] = entry[1]
+    obj[entry[0]] = entry[1]
   });
   
   return obj;
 }
+
 
 const objectToExport = {
   findName,
