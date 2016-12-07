a3b5552e335383ead9724c0508f829dc04769e3a
mlampedx <mlampesd@gmail.com>
2016-12-07 09:33:32 -0800

    phonebook.js complete

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -13,20 +13,33 @@
 * jazbooks are not always sorted...
 *
 * Develop a function that takes in a jazbook and a name, searches through the jazbook and
-* returns the persons phone number. If the person does not exists, return false.
+* returns the person's phone number. If the person does not exist, return false.
 *
 * How efficient can you make this?
 */
 
+// const jazbook = [
+//   ['alex','301-844-3421'],
+//   ['jae','301-844-1211'],
+//   ['david','301-844-0978'],
+//   ['travis','301-844-8505'],
+//   ['jasmine','1800-974-4539'],
+// ];
+
 //  return the number associated with the name in the jazbook
 function findName(jazbook, name) {
-
+  const phonebook = makePhoneBookObject(jazbook);
+  return phonebook[name] ? phonebook[name] : false;
 }
 
 // return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
-
+function makePhoneBookObject(jazbook) {
+  const phonebook = {};
+  jazbook.forEach(entry => phonebook[entry[0]] = entry[1]);
+  return phonebook;
 }
+
+// console.log(findName(jazbook, 'david'))
 
 const objectToExport = {
   findName,
