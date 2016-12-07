86a405deb0684e071edf0a15692f31a98fc212ae
David Wilson <w.dave.w@gmail.com>
2016-12-07 09:44:28 -0800

    phonebook

--- challenges/phonebook.js
+++ challenges/phonebook.js
@@ -18,19 +18,52 @@
 * How efficient can you make this?
 */
 
-//  return the number associated with the name in the jazbook
-function findName(jazbook, name) {
+//  sort jazbook in place
+let jazbookSorted = false;
+const memo = {};
 
+function sortJazbook(jazbook) {
+  jazbook.sort((a, b) => a[0] < b[0] ? -1 : 1);
 }
 
-// return an object literal representing the jazbook
-function makePhoneBookObject(jazbook){
+function binarySearchNameLookup(jazbook, name) {
+  const binarySearch = (start, end) => {
+    if (start > end) return false;
+    const mid = Math.floor((start + end) / 2);
+    if (jazbook[mid][0] === name) return jazbook[mid][1];
+    if (jazbook[mid][0] > name) return binarySearch(start, mid);
+    else return binarySearch(mid + 1, end);
+  }
+  return binarySearch(0, jazbook.length - 1);
+}
 
+// without using an object
+function findName(jazbook, name) {
+  if (memo[name]) return memo[name];
+  jazbookSorted ? void 0 : jazbook.sort();
+  const number = binarySearchNameLookup(jazbook, name);
+  return memo[name] = number, number;
+}
+
+// alternatively, could create hash table of phone numbers for constant lookup time
+// return an object literal representing the jazbook
+function makePhoneBookObject(jazbook) {
+  return jazbook.reduce((acc, current) => 
+    Object.assign(acc, { [current[0]]: current[1] }), {})
 }
 
 const objectToExport = {
   findName,
   makePhoneBookObject,
 };
+
+const jazbook = [
+  ['alex', '301-844-3421'],
+  ['jae', '301-844-1211'],
+  ['david', '301-844-0978'],
+  ['travis', '301-844-8505'],
+  ['jasmine', '1800-974-4539'],
+];
+
 
 module.exports = objectToExport;
