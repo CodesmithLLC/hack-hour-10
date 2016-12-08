505cea26ada1a5d800b39b90902ca4a51cf81c64
Will Galebach <willbach@gmail.com>
2016-12-08 09:20:36 -0800

    finished countStairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -3,7 +3,9 @@
  */
 
 function countStairs(n) {
-  return n < 0 ? 0 : n === 0 ? 1 : countStairs(n-1) + countStairs(n-2);
+  if(n < 1) return 0;
+  let find = n => n < 0 ? 0 : n === 0 ? 1 : find(n-1) + find(n-2);
+  return find(n);
 }
 
 module.exports = countStairs;
