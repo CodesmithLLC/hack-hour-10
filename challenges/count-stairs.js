b97282576fb38b0b797c4b258960482366b48c6e
Edward Guo <edward.h.guo@gmail.com>
2016-12-08 09:41:57 -0800

    countStairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -1,9 +1,16 @@
-/* You are faced by a staircase that is N steps high. You can take 1 or 2 steps at a time. Write
- * a function to calculate how many different ways you can go up the flight of stairs.
+/**
+ * Determine how many ways you can go up a
+ * flight of n stairs, either taking 1 or 2
+ * steps at a time.
+ *
+ * @param {number} n
+ * @return {number}
  */
-
 function countStairs(n) {
-
+  if (n === 0) return 1;
+  if (n === 1) return 1;
+  if (n === 2) return 2;
+  return countStairs(n - 1) + countStairs(n - 2);
 }
 
 module.exports = countStairs;
