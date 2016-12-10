f6da23ae184e559d8fa0f93b677e89032268a1e4
Edward Guo <edward.h.guo@gmail.com>
2016-12-10 09:47:53 -0800

    knight jumps

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -11,7 +11,20 @@
 // var str = "(4 5)"
 
 function knightjumps(str) {
-
+  const coords = str.replace(/[()]/g, '');
+  const x = +coords[0], y = +coords[2];
+  const upLeft = (x, y) => y + 2 <= 8 && x - 1 >= 1
+  const leftUp = (x, y) => y + 1 <= 8 && x - 2 >= 1
+  const upRight = (x, y) => y + 2 <= 8 && x + 1 <= 8
+  const rightUp = (x, y) => y + 1 <= 8 && x + 2 <= 8
+  const downLeft = (x, y) => y - 2 >= 1 && x - 1 >= 1
+  const leftDown = (x, y) => y - 1 >= 1 && x - 2 >= 1
+  const downRight = (x, y) => y - 2 >= 1 && x + 1 <= 8
+  const rightDown = (x, y) => y - 1 >= 1 && x + 2 <= 8
+  const moves = [
+    upLeft, leftUp, upRight, rightUp, downLeft, leftDown, downRight, rightDown
+  ]
+  return moves.reduce((moves, currMove) => currMove(x, y) ? moves + 1 : moves, 0)
 }
 
 module.exports = knightjumps;
