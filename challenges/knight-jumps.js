8d23ddf3b77163fa7375b09666aba6e7465d1177
Dan <dan.zot@gmail.com>
2016-12-10 09:36:08 -0800

    complete knight-jumps

--- challenges/knight-jumps.js
+++ challenges/knight-jumps.js
@@ -11,7 +11,32 @@
 // var str = "(4 5)"
 
 function knightjumps(str) {
-
+  const xyArr = str.slice(0, -1).slice(1).split(' ');
+  const xyStart = {
+    x: Number(xyArr[0]),
+    y: Number(xyArr[1])
+  }
+  const possibleMoves = ((xy) => {
+    const possibilities = [];
+    // top right
+    possibilities.push({ x: xy.x + 1, y: xy.y + 2 });
+    possibilities.push({ x: xy.x + 2, y: xy.y + 1 });
+    // bottom right
+    possibilities.push({ x: xy.x + 2, y: xy.y - 1 });
+    possibilities.push({ x: xy.x + 1, y: xy.y - 2 });
+    // bottom left
+    possibilities.push({ x: xy.x - 1, y: xy.y - 2 });
+    possibilities.push({ x: xy.x - 2, y: xy.y - 1 });
+    // top left
+    possibilities.push({ x: xy.x - 2, y: xy.y + 1 });
+    possibilities.push({ x: xy.x - 1, y: xy.y + 2 });
+    return possibilities;
+  })(xyStart);
+  const validMoves = possibleMoves.filter(xy => xy.x >= 1 && xy.x <= 8 && xy.y >= 1 && xy.y <= 8);
+  return validMoves.length;
 }
+
+console.log(knightjumps('(4 5)'))
+console.log(knightjumps('(0 0)'))
 
 module.exports = knightjumps;
