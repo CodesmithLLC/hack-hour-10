f78e64c5ede168e58c657432db6afa36f0a7d8a1
Will Galebach <willbach@gmail.com>
2016-12-13 09:35:14 -0800

    finished the PIN one

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -39,12 +39,35 @@ expectations = {
 
 */
 
-
-
-
 function getPINs(observed) {
-
+  if(!observed) return [];
+  let poss = {
+    1: ['1', '2', '4'],
+    2: ['1', '2', '3', '5'],
+    3: ['2', '3', '6'],
+    4: ['1', '4', '5', '7'],
+    5: ['2', '4', '5', '6', '8'],
+    6: ['3', '5', '6', '9'],
+    7: ['4', '7', '8'],
+    8: ['5', '7', '8', '9', '0'],
+    9: ['6', '8', '9'],
+    0: ['8', '0']
+  }
+  let results = [''];
+  for(let i = 0; i < observed.length; i++) {
+    var curr = observed[i];
+    results = results.reduce( (acc, cur) => {
+      poss[curr].forEach( (ele) => {
+        acc.push(cur + ele);
+      });
+      return acc;
+    }, [])
+  }
+  return results;
 }
+
+
+console.log(getPINs('').length);
 
 
 module.exports = getPINs
