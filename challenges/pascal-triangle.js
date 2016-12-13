98df4883cf6f249bf6a9fa61a5bfe561de62408d
George Norberg <george.norberg@gmail.com>
2016-12-12 21:16:32 -0800

    quadratic with for loops

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,33 +33,51 @@
 */
 
 function pascalTriangle(numRows) {
-  const memo = {};
-  function factorial(num) {
-    if (num === 0) return 1;
-    return num in memo ? memo[num] : memo[num] = num * factorial(num - 1);
-  }
-
-  const results = [];
-
-  function generatePascal(rowNumber) {
-    if (rowNumber < 0) return;
-    let k = 0;
-    const row = [];
-    while (rowNumber >= k) {
-      const numerator = factorial(rowNumber);
-      const denominator = factorial(rowNumber - k) * factorial(k);
-      row.push(numerator / denominator);
-      k += 1;
+  if (numRows === 0) return [];
+  const results = [[1]];
+  for (let i = 1; i <= numRows; i++) {
+    const row = [1];
+    for (let j = 1; j < i; j++) {
+      const lastRow = results[results.length - 1];
+      row.push(lastRow[j] + lastRow[j - 1]);
     }
 
+    row.push(1);
     results.push(row);
-    generatePascal(rowNumber - 1);
   }
 
-  generatePascal(numRows - 1);
-  return results.reverse();
+  return results;
 }
 
-// console.log(pascalTriangle(600));
+// console.log(pascalTriangle(6));
 
 module.exports = pascalTriangle;
+
+// MATH SOLUTION GREAT IF JUST NEED NTH ROW
+// function pascalTriangle(numRows) {
+//   const memo = {};
+//   function factorial(num) {
+//     if (num === 0) return 1;
+//     return num in memo ? memo[num] : memo[num] = num * factorial(num - 1);
+//   }
+
+//   const results = [];
+
+//   function generatePascal(rowNumber) {
+//     if (rowNumber < 0) return;
+//     let k = 0;
+//     const row = [];
+//     while (rowNumber >= k) {
+//       const numerator = factorial(rowNumber);
+//       const denominator = factorial(rowNumber - k) * factorial(k);
+//       row.push(numerator / denominator);
+//       k += 1;
+//     }
+
+//     results.push(row);
+//     generatePascal(rowNumber - 1);
+//   }
+
+//   generatePascal(numRows - 1);
+//   return results.reverse();
+// }
