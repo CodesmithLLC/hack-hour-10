efe69861904f3b521bdd8706af7c57b44496101a
ryanbas21 <ryanbas21@gmail.com>
2016-12-14 09:25:17 -0800

    save

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,7 +10,21 @@
 */
 
 function solveKnapsack(items, weightAvailable) {
+  if (!items) return 0;
+  let total = 0;
+  let value = 0;
+  items.sort(function (a, b) {
+      return b.value - a.value
+  });
 
+  items.reduce( (prev,curr) => {
+    total += curr.weight;
+    return (total <= weightAvailable) ? value += curr.value : value;
+  },{});
+  return value
 };
 
+let items = [{weight: 1, value : 100}, {weight: 2, value : 2}, {weight: 3, value : 5}];
+
+console.log(solveKnapsack(items,5))
 module.exports = solveKnapsack;
