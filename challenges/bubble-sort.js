102cf19f4e4191d12f2deefeb2d98bbbbf2342b3
ryanbas21 <ryanbas21@gmail.com>
2016-12-14 09:07:21 -0800

    save

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,8 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
 function bubbleSort(array) {
   let i = 0;
-  console.log(array);
   while (i < array.length){
     if (array[i] > array[i + 1]){
       let temp = array[i];
@@ -14,10 +12,12 @@ function bubbleSort(array) {
       i = 0;
     }else i++
   }
-  console.log(array);
   return array;
 }
 
 // bubbleSort([1,4,3,2,6,7,8]);
+let x = performance.now();
 bubbleSort([9,8,7,6,5,4,3,2,1]);
+let y = performance.now();
+console.log(y - x);
 module.exports = bubbleSort;
