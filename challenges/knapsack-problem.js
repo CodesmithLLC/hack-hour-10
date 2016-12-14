f7957f1e9ef4ad31fa61b81b72c2638c586de6d2
mlampedx <mlampesd@gmail.com>
2016-12-14 09:44:10 -0800

    knapsack-problem.js complete

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -10,7 +10,75 @@
 */
 
 function solveKnapsack(items, weightAvailable) {
+  if (!Array.isArray(items) || isNaN(weightAvailable)) { throw new Error ('Items must be an array and weightAvailable must be a number.') }
+  if (!items.length || weightAvailable === 0) { return 0 }
+  const possibleValues = [];
+  let sortedItems = items.sort((a, b) => b.value - a.value);
+  let currentWeightAvailable = weightAvailable;
+  let currentValue = 0;
+  return generateCombinations(sortedItems, currentWeightAvailable, currentValue);
 
+function generateCombinations(sortedItems, currentWeightAvailable, currentValue) {
+  if (!sortedItems.length) { return Math.max(...possibleValues) }
+    sortedItems.forEach(item => {
+      if (currentWeightAvailable >= item.weight) {
+        currentWeightAvailable -= item.weight;
+        currentValue += item.value;
+      }
+    })
+    possibleValues.push(currentValue);
+    return generateCombinations(sortedItems.slice(1), weightAvailable, 0);
+}
 };
+
+// var items = [{weight: 1, value : 3}, {weight: 2, value : 4}, {weight: 3, value : 5}];
+// console.log(solveKnapsack(items, 3))
+
+// Solution #1 - All one function
+
+// function solveKnapsack(items, weightAvailable) {
+//   if (!Array.isArray(items) || isNaN(weightAvailable)) { throw new Error ('Items must be an array and weightAvailable must be a number.') }
+//   const possibleValues = [];
+//   let sortedItems = items.sort((a, b) => b.value - a.value);
+//   let currentWeightAvailable = weightAvailable;
+//   let currentValue = 0;
+
+//   while (sortedItems.length) {
+//     sortedItems.forEach(item => {
+//       if (currentWeightAvailable >= item.weight) {
+//         currentWeightAvailable -= item.weight;
+//         currentValue += item.value;
+//       }
+//     })
+//     possibleValues.push(currentValue);
+//     currentWeightAvailable = weightAvailable;
+//     currentValue = 0;
+//     sortedItems = sortedItems.slice(1);
+//   }
+//   return Math.max(...possibleValues);
+// };
+
+// Solution #2 - Helper function
+
+// function solveKnapsack(items, weightAvailable) {
+//   if (!Array.isArray(items) || isNaN(weightAvailable)) { throw new Error ('Items must be an array and weightAvailable must be a number.') }
+//   const possibleValues = [];
+//   let sortedItems = items.sort((a, b) => b.value - a.value);
+//   let currentWeightAvailable = weightAvailable;
+//   let currentValue = 0;
+//   return generateCombinations(sortedItems, currentWeightAvailable, currentValue);
+
+// function generateCombinations(sortedItems, currentWeightAvailable, currentValue) {
+//   if (!sortedItems.length) { return Math.max(...possibleValues) }
+//     sortedItems.forEach(item => {
+//       if (currentWeightAvailable >= item.weight) {
+//         currentWeightAvailable -= item.weight;
+//         currentValue += item.value;
+//       }
+//     })
+//     possibleValues.push(currentValue);
+//     return generateCombinations(sortedItems.slice(1), weightAvailable, 0);
+// }
+// };
 
 module.exports = solveKnapsack;
