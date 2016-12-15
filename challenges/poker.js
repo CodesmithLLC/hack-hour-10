a6efbfdff003c1058e52dacbcc8f4d09c00ca46b
Michael Sotkin <michael.sotkin@gmail.com>
2016-12-15 10:09:18 -0800

    finished poker

--- challenges/poker.js
+++ challenges/poker.js
@@ -19,7 +19,62 @@
 */
 
 function poker(hand1, hand2) {
+  let score1 = {points: null, highCard: null};
+  let score2 = {points: null, highCard: null};
 
+  function handCounter(hand, score) {
+    let count = hand.reduce((acc, curr) => {
+      if (!acc[curr]) acc[curr] = 1;
+      else acc[curr]++;
+      return acc;
+    }, {});
+
+    for (let prop in count) {
+      if (count[prop] === 4) {
+        score.points = 7;
+        break;
+      } else if (count[prop] === 3) {
+        for (let key in count) {
+          if (count[key] === 2) {
+            score.points = 6;
+            break;
+          } else {
+            score.points = 4;
+          }
+        }
+      } else if (count[prop] === 2) {
+        for (let key in count) {
+          if (count[key] === 2 && key !== prop) {
+            score.points = 3;
+            break;
+          } else {
+            score.points = 2;
+          }
+        }
+      }
+    }
+
+    if (!score.points) {
+      let arr = Object.keys(count).map(num => Number(num)).sort((a,b) => a - b);
+      for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1] + 1) {
+          score.points = 1;
+          break;
+        }
+        score.points = 5;
+      }
+    }
+  }
+  handCounter(hand1, score1);
+  handCounter(hand2, score2);
+
+  if (score1.points > score2.points) {
+    return "Player 1 wins";
+  } else if (score1.points === score2.points) {
+    return "Draw";
+  } else {
+    return "Player 2 wins";
+  }
 }
 
 module.exports = poker;
