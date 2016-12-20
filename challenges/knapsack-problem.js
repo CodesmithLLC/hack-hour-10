7a22800ffde107d560a45871ef3ffae3d10df09f
Edward Park <ed.sh.park@gmail.com>
2016-12-20 00:01:29 -0800

    solved knapsack

--- challenges/knapsack-problem.js
+++ challenges/knapsack-problem.js
@@ -9,56 +9,23 @@
   solveKnapsack(items, 5); // returns 9 (from items[1] and items[2])
 */
 
-function solveKnapsack2(items, weightAvailable) {
-  let maxVal;
-
-  const recurse = (array, newWeight) => {
-    if (!items.length) return false;
-
-    for (let i = 0; i < items.length; ++i) {
-      if (weightAvailable - items[i].weight !== 0) {
-        items.splice(i, 1);
-        recurse(items, weightAvailable - items[i].weight);
-      }
-    }
-  }
-  
-  recurse(items, weightAvailable);
-  return maxVal;
-};
 
 let items = [{ weight: 1, value: 3 }, { weight: 5, value: 2 }, { weight: 2, value: 4 }, { weight: 3, value: 5 }];
 
-console.log(solveKnapsack(items));
 
+const solveKnapsack = (items, weightAvailable, value = 0) => {
+  let newWeight, newValue;
 
-module.exports = solveKnapsack;
-
-
-const solveKnapsack = (items, weightAvailable, valueSoFar = 0) => {
-  // 2 cases: 1 includes changing value and weight.  Other does not.
-  
-  if (weightAvailable < 0) {
-    // do something
-    // items exceed available weight
-    return -Infinity;
-  }
-
-  // if (weightAvailable === 0) {
-  //   // do something
-  //   // successful base case - you've filled up your bag
-
-  // }
-
-  // if (!items.length) {
-  //   // do something
-  //   // successfull base case - all the items don't amount to your weight limit
-  // }
+  if (weightAvailable < 0) return -Infinity;
 
   if (!items.length || weightAvailable === 0) {
-    // do something
+    return value;
   }
 
-  solveKnasack(items.slice(1), weightAvailable)  
-  solveKnasack(items.slice(1), )  
+  newWeight = weightAvailable - items[0].weight;
+  newValue = value + items[0].value;
+
+  return Math.max(solveKnapsack(items.slice(1), weightAvailable, value), solveKnapsack(items.slice(1), newWeight, newValue));
 }
+
+module.exports = solveKnapsack;
