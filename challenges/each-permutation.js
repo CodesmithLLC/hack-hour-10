8d8efc7689c868a331f363b525ca72198b100b54
mlampedx <mlampesd@gmail.com>
2016-12-21 09:39:03 -0800

    each-permutation.js complete

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -17,9 +17,37 @@ eachPermutation([1, 2, 3], function(perm) {
 */
 
 function eachPermutation(arr, callback) {
+  if (!Array.isArray(arr)) { throw new Error('Parameter 1 must be an array.') }
+  if (typeof callback !== 'function') { throw new Error('Parameter 2 must be a function.') }
+  const permutationArr = permutator(arr);
+  return permutationArr.forEach(perm => callback(perm));
+
+function permutator(inputArr) {
+  const results = [];
+
+  function permute(arr, memo) {
+    let cur
+    memo = memo || [];
+
+    for (let i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
 
 }
 
-
+// console.log(eachPermutation([1,2,3], function(perm) {
+//   console.log(perm)
+// }))
 
 module.exports = eachPermutation;
