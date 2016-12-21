31dac0723a9ba097964467540aa3fda9e7bcd190
Martin-Ting <martinhting@gmail.com>
2016-12-21 09:47:17 -0800

    each permutation

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -16,10 +16,39 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
 
+// function eachPermutation(arr, callback, curr = [], layer = 0) {
+//   console.log(`called`);
+//   console.log(arr);
+//   console.log(curr);
+//   if(arr.length === 0){
+//     console.log(`=======================> callback(${curr})`);
+//     return;
+//   }
+//   arr.forEach((elem, idx) => {
+//     eachPermutation([...arr.slice(0,idx), ...arr.slice(idx+1)], callback, [...curr, ...arr.slice(idx,1)]);
+//   });
+// }
+
+function eachPermutation(arr, callback, curr = [], layer = 0) {
+  if(arr.length === 1){
+    // console.log(`===============================>${[...curr, ...arr]}`);
+    callback([...curr, ...arr]);
+  }else{
+    arr.forEach((elem, idx) => {
+      // console.log(`layer ${layer} arr =============================    idx ${idx}`);
+      // console.log(`arr`);
+      // console.log(arr);
+      // console.log(`sliced`);
+      // console.log([...arr.slice(0,idx), ...arr.slice(idx+1)])
+      // console.log(`curr`);
+      // console.log([...arr.slice(idx,1), ...curr]);
+      eachPermutation([...arr.slice(0,idx), ...arr.slice(idx+1)], callback, [...arr.slice(idx,idx+1), ...curr], layer + 1);
+    });
+  }
 }
-
-
+eachPermutation([1, 2, 3], function(perm) {
+  console.log(perm)
+});
 
 module.exports = eachPermutation;
