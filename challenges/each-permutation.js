316cec2da75ad1fdbb3d942da97ba9c788cf1fdf
Dean Edwards <dmedwards4@gmail.com>
2016-12-21 10:42:12 -0800

    each-permutation.js

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -1,8 +1,10 @@
 /*
-Write a function that takes two arguments, an array and a callback. The callback will be called once for each permutation
+Write a function that takes two arguments, an array and a callback.
+The callback will be called once for each permutation
 of the elements of the array, where the permutation is passed as an argument.
 
-Permutations are all the possible rearrangements of the elements. For this problem, assume each permutation must include
+Permutations are all the possible rearrangements of the elements.
+For this problem, assume each permutation must include
 every element from the array. Permutations must not repeat any of the array elements.
 
 eachPermutation([1, 2, 3], function(perm) {
@@ -16,10 +18,19 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
-
+function eachPermutation(array, callback) {
+  (function perm(arr, parr) {
+    if (parr.length === 0) {
+      callback(arr);
+      return;
+    }
+    parr.forEach((ele, i) => {
+      return perm(arr.concat(ele), parr.slice(0, i).concat(parr.slice(i + 1)));
+    });
+  })([], array);
+  return;
 }
 
-
-
-module.exports = eachPermutation;
+// console.log(eachPermutation([1, 2, 3], function (perm) {
+//   console.log(perm)
+// }));
