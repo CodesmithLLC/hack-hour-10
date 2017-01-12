0ce17eaaf496acf85d8a7f7655558a15fff3ef41
Thai-Duong Nguyen <soleil@uw.edu>
2017-01-12 01:18:16 -0800

    Thai's unique-number optimized

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,10 +10,7 @@
  *
  */
 function uniqueNumber(array) {
-    const sorted = array.sort()
-    for (let i = 0; i < sorted.length; i += 2) {
-        if (sorted[i] !== sorted[i+1]) return sorted[i]
-    }
+  return array.reduce((a, c) => a ^ c)
 }
 
 module.exports = uniqueNumber;
