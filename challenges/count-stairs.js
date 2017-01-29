611179d5b598a36922dff771238517aeaa98a410
Thai-Duong Nguyen <soleil@uw.edu>
2017-01-29 02:53:56 -0800

    Thai's count-stairs optimized

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -4,6 +4,7 @@
 
 function countStairs(n, memo = {}) {
   if (memo[n] !== undefined) return memo[n]
+  if (n < 0) return 0
   if (n < 2) return 1
   else return memo[n] = countStairs(n - 1, memo) + countStairs(n - 2, memo)
 }
