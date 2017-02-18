21241240c2c175d1d28ffd9bdd9df46d0cf9b50a
George Norberg <george.norberg@gmail.com>
2017-02-17 17:39:50 -0800

    testing instanceof

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -12,23 +12,26 @@ function BinaryTree(val) {
 
 // MAIN IDEA: Recursively traverse left and right branches while comparing to changing min and max
 function validBST(BST) {
-  // Create validation helper function
-  // INPUT: tree, min, max
-  // OUTPUT: boolean
+  if (BST instanceof BinaryTree === false) return 'bad input';
+  
+  /* Create validation helper function
+   * INPUT: tree, min, max
+   * OUTPUT: boolean
+   */
   function isValid(tree, min, max) {
     // Base case: if tree is null, return true as we have reached a leaf successfully
     if (!tree) return true;
 
-    // Is current value greater than minimum handed down from previous call?
+    // Current value must be greater than minimum handed down from previous call
     if (tree.value > min &&
         
-        // Is current value less than maximum handed down from previous call?
+        // Current value must be less than maximum handed down from previous call
         tree.value < max &&
       
-        // If both previous checks pass, run isValid on left branch with same min and max as current value
+        // If both previous checks pass, run isValid on left branch with same min but max = current value
         isValid(tree.left, min, tree.value) &&
         
-        // If both previous checks pass, run isValid on left branch with same min and max as current value
+        // If both previous checks pass, run isValid on left branch with same max but min = current value
         isValid(tree.right, tree.value, max)) {
       
         // If all recursive calls return true, then entire tree is valid, so return true!
@@ -39,59 +42,75 @@ function validBST(BST) {
     return false;
   }
 
+  // Begin recursive search with BST, -Infinity and Infinity
   return isValid(BST, -Infinity, Infinity);
 }
 
 
-//   const tree1 = new BinaryTree(10);
-//   tree1.left = new BinaryTree(5);
-//   tree1.right = new BinaryTree(14);
-//   tree1.left.left = new BinaryTree(3);
-//   tree1.left.right = new BinaryTree(6);
+  const tree1 = new BinaryTree(10);
+  tree1.left = new BinaryTree(5);
+  tree1.right = new BinaryTree(14);
+  tree1.left.left = new BinaryTree(3);
+  tree1.left.right = new BinaryTree(6);
 
-// /* TREE 1
-//              10
-//             /  \
-//            5    14
-//           / \     
-//          3   6   
-// */
+/* TREE 1
+             10
+            /  \
+           5    14
+          / \     
+         3   6   
+*/
 
-//   const tree2 = new BinaryTree(10);
-//   tree2.left = new BinaryTree(5);
-//   tree2.right = new BinaryTree(14);
-//   tree2.left.left = new BinaryTree(3);
-//   tree2.left.right = new BinaryTree(6);
-//   tree2.right.left = new BinaryTree(9);
+  const tree2 = new BinaryTree(10);
+  tree2.left = new BinaryTree(5);
+  tree2.right = new BinaryTree(14);
+  tree2.left.left = new BinaryTree(3);
+  tree2.left.right = new BinaryTree(6);
+  tree2.right.left = new BinaryTree(9);
 
-// /* TREE 2
-//                10
-//              /    \
-//            5       14
-//           / \     /
-//          3   6   9
-// */
+/* TREE 2
+               10
+             /    \
+           5       14
+          / \     /
+         3   6   9
+*/
 
-//   const tree3 = new BinaryTree(10);
-//   tree3.left = new BinaryTree(5);
-//   tree3.right = new BinaryTree(14);
-//   tree3.left.left = new BinaryTree(3);
-//   tree3.left.right = new BinaryTree(6);
-//   tree3.left.right.right = new BinaryTree(11);
+  const tree3 = new BinaryTree(10);
+  tree3.left = new BinaryTree(5);
+  tree3.right = new BinaryTree(14);
+  tree3.left.left = new BinaryTree(3);
+  tree3.left.right = new BinaryTree(6);
+  tree3.left.right.right = new BinaryTree(11);
 
-// /* TREE 3
-//              10
-//             /  \
-//            5    14
-//           / \     
-//          3   6
-//               \
-//                11
-// */
+/* TREE 3
+             10
+            /  \
+           5    14
+          / \     
+         3   6
+              \
+               11
+*/
 
-// console.log(validBST(tree1));
-// console.log(validBST(tree2));
-// console.log(validBST(tree3));
+const tree4 = new BinaryTree(10);
+
+console.log(validBST(tree1)); // true
+console.log(validBST(tree2)); // false
+console.log(validBST(tree3)); // false
+console.log(validBST(tree4)); // true
+console.log(validBST(null)); // bad input
+console.log(validBST('not an object')); // bad input
 
 
 module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+
+// console.log(Object.prototype.toString.call(3));
+// console.log(Object.prototype.toString.call(NaN));
+// console.log(Object.prototype.toString.call('test'));
+// console.log(Object.prototype.toString.call([]));
+// console.log(Object.prototype.toString.call({}));
+// console.log(Object.prototype.toString.call(null));
+// console.log(Object.prototype.toString.call(undefined));
+// console.log(Object.prototype.toString.call(/[a-z]/));
+// console.log(Object.prototype.toString.call(new Date()));
