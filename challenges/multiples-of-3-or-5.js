49c1c789454a9e8b676258f8c5c62e162574788a
Thai-Duong Nguyen <soleil@uw.edu>
2017-03-04 02:26:00 -0800

    Thai's multiples-of-3-or-5 better variable names

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,14 +8,14 @@
 function sumMultiples3Or5Below1000() {
 	const limit = 9;
 	const nums = [3, 5];
-	let lcd = 1;
+	let lcm = 1;
 	let sum = 0;
 	nums.forEach(n => {
-		lcd *= n;
+		lcm *= n;
 		const mults = Math.floor(limit / n);
 		sum += (mults * n + n) / 2 * mults;
 	});
-	sum -= Math.floor(limit / lcd) * lcd;
+	sum -= Math.floor(limit / lcm) * lcm;
 	return sum;
 }
 
@@ -24,14 +24,14 @@ function sumMultiples3Or5Below1000() {
 function sumMultiplesXOrYBelowZ(x, y, z) {
 	const limit = z - 1;
 	const nums = [x, y];
-	let lcd = 1;
+	let lcm = 1;
 	let sum = 0;
 	nums.forEach(n => {
-		lcd *= n;
+		lcm *= n;
 		const mults = Math.floor(limit / n);
 		sum += (mults * n + n) / 2 * mults;
 	});
-	sum -= Math.floor(limit / lcd) * lcd;
+	sum -= Math.floor(limit / lcm) * lcm;
 	return sum;
 }
 
