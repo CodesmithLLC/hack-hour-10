d426a0772a0902162fdebe002f1b6f5b8fcc444c
Thai-Duong Nguyen <soleil@uw.edu>
2017-03-15 09:33:05 -0700

    Thai's doubly-linked-list

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -3,47 +3,48 @@ Create a doubly linked list with an add and remove method
  */
 
 function LinkedList() {
-    this.head = null;
-    this.tail = null;
+	this.head = null;
+	this.tail = null;
 }
 
 function Node(val) {
-    this.val = val;
-    this.next = null;
-    this.prev = null;
+	this.val = val;
+	this.next = null;
+	this.prev = null;
 }
 
 /*
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function (val) {
-    if (!this.head) {
-        this.head = new Node(val)
-        this.tail = this.head
-    }
-    else {
-        this.tail.next = new Node(val)
-        this.tail.next.prev = this.tail
-        this.tail = this.tail.next
-    }
+	if (!this.head) {
+		this.head = new Node(val);
+		this.tail = this.head;
+	}
+	else {
+		this.tail.next = new Node(val);
+		this.tail.next.prev = this.tail;
+		this.tail = this.tail.next;
+	}
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function (val) {
-    if (this.head.val === val) this.head = this.head.next
-    else {
-        let cur = this.head.next
-        do {
-            if (cur.val === val) {
-                cur.prev.next = cur.next
-                if (cur !== this.tail) cur.next.prev = cur.prev
-                break
-            }
-            cur = cur.next
-        } while (cur)
-    }
+	if (!this.head.next) this.head = null;
+	if (this.head.val === val) this.head = this.head.next;
+	else {
+		let cur = this.head.next;
+		do {
+			if (cur.val === val) {
+				cur.prev.next = cur.next;
+				if (cur !== this.tail) cur.next.prev = cur.prev;
+				break;
+			}
+			cur = cur.next;
+		} while (cur);
+	}
 };
 
 module.exports = LinkedList;
